4ce82bacd4f7909a3029bde06c05d4e99c1b2add
Bryce Lanham <blanham@gmail.com>
2011-10-30 17:20:25 -0700

    added kernel memory functions

---
+++ src/include/kernel/memory.h
@@ -0,0 +1,10 @@
+#ifndef C_OS_MEMORY_H
+#define C_OS_MEMORY_H
+#include <kernel/types.h>
+size_t kstrlen(uint8_t *str);
+uint8_t *kmemcpy(uint8_t *dest, uint8_t *src, size_t count);
+uint16_t *kmemcpyw(uint16_t *dest, uint16_t *src, size_t count);
+uint8_t *kmemset(uint8_t *dest, uint8_t val, size_t count);
+uint16_t *kmemsetw(uint16_t *dest, uint16_t val, size_t count);
+#endif
+
---
+++ src/lib/kernel/memory.c
@@ -0,0 +1,44 @@
+#include <kernel/memory.h>
+size_t kstrlen(uint8_t *str)
+{
+	uint8_t *tmp = str;
+	while(*tmp != '\0');
+		tmp++;
+	return tmp - str;
+
+}
+
+uint8_t *kmemcpy(uint8_t *dest, uint8_t *src, size_t count)
+{
+	uint8_t *tmp = dest;
+	while(count-- != 0)
+		*dest++ = *src++;
+
+	return tmp;
+}
+uint16_t *kmemcpyw(uint16_t *dest, uint16_t *src, size_t count)
+{
+	uint16_t *tmp = dest;
+	while(count-- != 0)
+		*dest++ = *src++;
+
+	return tmp;
+}
+
+uint8_t *kmemset(uint8_t *dest, uint8_t val, size_t count)
+{
+	uint8_t *tmp = dest;
+	while(count-- != 0)
+		*dest++ = val;
+
+	return tmp;
+}
+
+uint16_t *kmemsetw(uint16_t *dest, uint16_t val, size_t count)
+{
+	uint16_t *tmp = dest;
+	while(count-- != 0)
+		*dest++ = val;
+
+	return tmp;
+}
