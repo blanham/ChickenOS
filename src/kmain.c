50e0c2aefec318db5bcf0894ecc7f9a9014ff2f3
Bryce Lanham <blanham@gmail.com>
2011-10-30 17:25:28 -0700

    cleaned up kmain, removed some useless lines

--- src/kmain.c
+++ src/kmain.c
@@ -12,9 +12,10 @@ void panic(char *msg)
 
 	while(1);
 }
-
-void kmain( void* mbd, uint32_t magic )
+#define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
+void kmain(uint32_t mbd, uint32_t magic)
 {
+	struct multiboot_info *mb;
    	if ( magic != 0x2BADB002 )
    	{
       /* Something went not according to specs. Print an error */
@@ -23,23 +24,21 @@ void kmain( void* mbd, uint32_t magic )
 		puts("Bad magic number, halting\r");
 		return;
    	}
-	struct multiboot_info_t *mb = (struct multiboot_info_t *)mbd;
-	mb = mb;
-   /* You could either use multiboot.h */
-   /* (http://www.gnu.org/software/grub/manual/multiboot/multiboot.html#multiboot_002eh) */
-   /* or do your offsets yourself. The following is merely an example. */ 
-   //char * boot_loader_name =(char*) ((long*)mbd)[16];
+ 	mb = (struct multiboot_info *)mbd;
+	
+	/* begin initializations */	
 	console_init();
+
+
+
+	if (CHECK_FLAG (mb->flags, 2))
+    	printf ("cmdline = %s\n", (char *) mb->cmdline);
+	
 	for(int i = 0; i < 43; i++)
 	{
 		printf("hello, world %b\n",i);
 		printf("hello, world %i\n",i);
 		printf("hello, world %x\n",i);
 	}
-/*	int i = 0xDEADBEEF;
-	int j = 1234567890;
-	int k = 42;
-	printf("hello, %x %i %b %c %sworld\n",i,j,k,'c',"fuck");
-*/
-	panic("kmain returned\0");
+	panic("kmain returned");
 }
