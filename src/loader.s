568734088adc5257655b31b55dffba2fd056f3d4
Bryce Lanham <blanham@gmail.com>
2011-11-16 13:04:35 -0800

    made changes so kernel is loaded in upper half of (virtual) address space

--- src/loader.s
+++ src/loader.s
@@ -1,38 +1,105 @@
-global loader                           ; making entry point visible to linker
- 
-extern kmain                            ; kmain is defined in kmain.cpp
- 
+
+global _loader                          ; Make entry point visible to linker.
+extern kmain                            ; _main is defined elsewhere
+extern end 
 ; setting up the Multiboot header - see GRUB docs for details
-MODULEALIGN equ  1<<0                   ; align loaded modules on page boundaries
-MEMINFO     equ  1<<1                   ; provide memory map
+MODULEALIGN equ  1<<0             ; align loaded modules on page boundaries
+MEMINFO     equ  1<<1             ; provide memory map
 FLAGS       equ  MODULEALIGN | MEMINFO  ; this is the Multiboot 'flag' field
-MAGIC       equ    0x1BADB002           ; 'magic number' lets bootloader find the header
-CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum required
+MAGIC       equ    0x1BADB002     ; 'magic number' lets bootloader find the header
+CHECKSUM    equ -(MAGIC + FLAGS)  ; checksum required
+ 
+; This is the virtual base address of kernel space. It must be used to convert virtual
+; addresses into physical addresses until paging is enabled. Note that this is not
+; the virtual address where the kernel image itself is loaded -- just the amount that must
+; be subtracted from a virtual address to get a physical address.
+KERNEL_VIRTUAL_BASE equ 0xC0000000                  ; 3GB
+KERNEL_PAGE_NUMBER equ (KERNEL_VIRTUAL_BASE >> 22)  ; Page directory index of kernel's 4MB PTE.
+ 
+ 
+section .data
+align 4096
+BootPageDirectory:
+    ; This page directory entry identity-maps the first 4MB of the 32-bit physical address space.
+    ; All bits are clear except the following:
+    ; bit 7: PS The kernel page is 4MB.
+    ; bit 1: RW The kernel page is read/write.
+    ; bit 0: P  The kernel page is present.
+    ; This entry must be here -- otherwise the kernel will crash immediately after paging is
+    ; enabled because it can't fetch the next instruction! It's ok to unmap this page later.
+    dd 0x00000083
+    times (KERNEL_PAGE_NUMBER - 1) dd 0                 ; Pages before kernel space.
+    ; This page directory entry defines a 4MB page containing the kernel.
+    dd 0x00000083
+    times (1024 - KERNEL_PAGE_NUMBER - 1) dd 0  ; Pages after the kernel image.
+ 
  
 section .text
- 
 align 4
+MultiBootHeader:
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
  
-; reserve initial kernel stack space
-STACKSIZE equ 0x4000                    ; that's 16k.
+; reserve initial kernel stack space -- that's 16k.
+STACKSIZE equ 0x4000
  
-loader:
-    mov  esp, stack + STACKSIZE         ; set up the stack
-    push eax                            ; Multiboot magic number
-    push ebx                            ; Multiboot info structure
+_loader:
+    ; NOTE: Until paging is set up, the code must be position-independent and use physical
+    ; addresses, not virtual ones!
+    mov ecx, (BootPageDirectory - KERNEL_VIRTUAL_BASE)
+    mov cr3, ecx                                        ; Load Page Directory Base Register.
  
-    call kmain                          ; call kernel proper
+    mov ecx, cr4
+    or ecx, 0x00000010                          ; Set PSE bit in CR4 to enable 4MB pages.
+    mov cr4, ecx
  
-    cli
-.hang:
-    hlt                                 ; halt machine should kernel return
-    jmp  .hang
+    mov ecx, cr0
+    or ecx, 0x80000000                          ; Set PG bit in CR0 to enable paging.
+    mov cr0, ecx
+ 
+    ; Start fetching instructions in kernel space.
+    lea ecx, [StartInHigherHalf]
+    jmp ecx                                                     ; NOTE: Must be absolute jump!
+ 
+StartInHigherHalf:
+    ; Unmap the identity-mapped first 4MB of physical address space. It should not be needed
+    ; anymore.
+    mov dword [BootPageDirectory], 0
+    invlpg [0]
+ 
+    ; NOTE: From now on, paging should be enabled. The first 4MB of physical address space is
+    ; mapped starting at KERNEL_VIRTUAL_BASE. Everything is linked to this address, so no more
+    ; position-independent code or funny business with virtual-to-physical address translation
+    ; should be necessary. We now have a higher-half kernel.
+    mov esp, stack+STACKSIZE           ; set up the stack
+    push eax                           ; pass Multiboot magic number
+ 
+    ; pass Multiboot info structure -- WARNING: This is a physical address and may not be
+    ; in the first 4MB!
+    push ebx
+ 
+    call  kmain                  ; call kernel proper
+    hlt                          ; halt machine should kernel return
+[global gdt_flush] ; make 'gdt_flush' accessible from C code
+[extern gp]        ; tells the assembler to look at C code for 'gp'
+ 
+; this function does the same thing of the 'start' one, this time with
+; the real GDT
+gdt_flush:
+	lgdt [gp]
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	jmp 0x08:flush2
+ 
+flush2:
+	ret 
  
 section .bss
- 
-align 4
+align 32
 stack:
-    resb STACKSIZE                      ; reserve 16k stack on a doubleword boundary
+    resb STACKSIZE      ; reserve 16k stack on a quadword boundary
