25ac9967dc793ee7d0a2051fcc2f7fd00a487238
Bryce Lanham <blanham@gmail.com>
2011-11-16 13:06:02 -0800

    inits PICs, sets up idt for both isrs and irqs, and sets up the keyboard

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,74 +1,163 @@
+/*	ChickenOS - interrupt.c
+ *  Borrows from Bran and JamesM's tutorials and
+ *  the pintos teaching os
+ */
+
 #include <kernel/interrupt.h>
 #include <stdio.h>
 #include <kernel/console.h>
 #include <kernel/memory.h>
 #include <kernel/gdt.h>
 #include <kernel/hw.h>
+#include "i386-defs.h"
 
-#define NUM_INTRS 256
-/* IDT flags */
-#define IDT_FLAG_BASE 0x0E//always
-#define IDT_FLAG_PRESENT 0x80
-#define IDT_FLAG_RING0   0x00
-#define IDT_FLAG_RING1   0x20
-#define IDT_FLAG_RING2   0x40
-#define IDT_FLAG_RING3	 0x60
-/* structs taken from James Molloy's kernel tutorial */
-struct idt_entry_struct
-{
-   uint16_t base_lo;             // The lower 16 bits of the address to jump to when this interrupt fires.
-   uint16_t sel;                 // Kernel segment selector.
-   uint8_t  always0;             // This must always be zero.
-   uint8_t  flags;               // More flags. See documentation.
-   uint16_t base_hi;             // The upper 16 bits of the address to jump to.
-} __attribute__((packed));
-typedef struct idt_entry_struct idt_entry_t;
+#define NUM_IRQS 16
+#define NUM_ISRS 32
 
-// A struct describing a pointer to an array of interrupt handlers.
-// This is in a format suitable for giving to 'lidt'.
-struct idt_ptr_struct
-{
-   uint16_t limit;
-   uint32_t base;                // The address of the first element in our idt_entry_t array.
-} __attribute__((packed));
-typedef struct idt_ptr_struct idt_ptr_t;
 idt_entry_t idt_table[NUM_INTRS];
 idt_ptr_t	idt_ptr;
+intr_handler *intr_handlers[NUM_INTRS];
+
 
 extern void idt_flush(uint32_t);
-static void idt_build_entry(idt_entry_t *entry, void *func, uint16_t sel, uint8_t flags);
-/*
-void (*isrs[])(void) = {
-isr0,  isr1, isr2, isr3, isr4, 
-isr5,  isr6, isr7, isr8, isr9, 
-isr10, isr11, isr12, isr13, isr14, 
-isr15, isr16, isr17, isr18, isr19, 
-isr20, isr21, isr22, isr23, isr24, 
-isr25, isr26, isr27, isr28, isr29, 
-isr30, isr31};*/
-void test()
+static void idt_build_entry(idt_entry_t *entry, uint32_t func, uint16_t sel, uint8_t flags);
+
+void pic_init();
+
+void pic_unmask(int irq);
+void pic_send_end(int irq);
+
+void void_handler(struct registers *regs)
 {
+	if(regs->int_no < NUM_ISRS)
+		printf("unhandled interrupt %i\n", regs->int_no);
+	else
+		printf("unhandled irq %i\n",regs->int_no - NUM_ISRS);
 }
+
 void interrupt_init()
 {
-	idt_ptr.base = (uint32_t)idt_table;
+	
+	idt_ptr.base = (uint32_t)&idt_table;
 	idt_ptr.limit = sizeof(idt_entry_t)*256 - 1;
+	
 	kmemset((uint8_t *)idt_table, 0, sizeof(idt_entry_t) * NUM_INTRS);
-	uint8_t flags = IDT_FLAG_BASE | IDT_FLAG_RING0;
-	uint16_t sel = 0x08;
-	for(int i = 0; i < NUM_INTRS; i++)
-		idt_build_entry(&idt_table[i], test, sel, flags);
-//	for(int i = 0; i < 32; i++)
-//		idt_build_entry(&idt_table[i], isrs[i], sel, flags);
 
+	pic_init();
+
+	for(int i = 0; i < NUM_ISRS + NUM_IRQS; i++)
+		idt_build_entry(&idt_table[i], (uint32_t)isrs[i], 0x08, IDT_FLAG_BASE | IDT_FLAG_PRESENT);
+	for(int i = 0; i < NUM_INTRS; i++)
+		interrupt_register(i, void_handler);
+	
 	idt_flush((uint32_t)&idt_ptr);
+
+	asm volatile("sti");
 }
 
-static void idt_build_entry(idt_entry_t *entry, void *func, uint16_t sel, uint8_t flags)
+void interrupt_register(int irq, intr_handler *handler)
+{
+	intr_handlers[irq] = handler;
+	if(irq > NUM_ISRS)
+		pic_unmask(1);
+}
+
+void interrupt_handler(struct registers *regs)
+{
+	intr_handler *handler = intr_handlers[regs->int_no];
+	if(handler){
+		handler(regs); 
+	} else {
+		printf("something wrong in interrupt.c\n");
+	}
+	if(regs->int_no > NUM_ISRS)
+		pic_send_end(regs->int_no - NUM_ISRS);
+}
+
+void pic_init()
+{
+	outb(PIC1_DATA, 0xff);
+	outb(PIC2_DATA, 0xff);
+
+	/* starts init */
+	outb(PIC1_CMD, 0x11);
+ 	outb(PIC2_CMD, 0x11);
+	/* set offsets */
+	outb(PIC1_DATA, 0x20);
+	outb(PIC2_DATA, 0x28);
+	/* more init */
+	outb(PIC1_DATA, 0x04);
+	outb(PIC2_DATA, 0x02);
+	/* set 8086 mode */
+	outb(PIC1_DATA, 0x01);
+	outb(PIC2_DATA, 0x01);
+	/* finish init */
+	outb(PIC1_DATA, 0x01);//disables system timer for now
+	outb(PIC2_DATA, 0x0);
+}
+void pic_send_end(int irq)
+{
+	if( irq >= 8)
+		outb(PIC2_CMD, PIC_EOI);
+
+	outb(PIC1_CMD, PIC_EOI);
+
+}
+void pic_mask(int irq)
+{
+	uint8_t val;
+	uint16_t port;
+
+	if(irq < 8)
+		port = PIC1_DATA;
+	else{
+		port = PIC2_DATA;
+		irq -= 8;
+	}
+
+	val = inb(port) | (1 << irq);
+	outb(port, val);
+
+
+}
+void pic_unmask(int irq)
+{
+	uint8_t val;
+	uint16_t port;
+
+	if(irq < 8)
+		port = PIC1_DATA;
+	else{
+		port = PIC2_DATA;
+		irq -= 8;
+	}
+
+	val = inb(port) & ~(1 << irq);
+	outb(port, val);
+
+
+}
+static void idt_build_entry(idt_entry_t *entry, uint32_t func, uint16_t sel, uint8_t flags)
 {
 	entry->sel = sel;
 	entry->flags = flags;//needs to be ORed with 0x60 once we get to user mode
+	entry->always0 = 0;
 	entry->base_lo = ((uint32_t)func) & 0xFFFF;
 	entry->base_hi = (((uint32_t)func) >> 16) & 0xFFFF;
 } 
+
+
+void test()
+{
+const char s[] = "Shutdown";
+	const char *p;
+for (p = s; *p != '\0'; p++)
+    outb (0x8900, *p);
+	asm volatile ("hlt");
+
+
+
+}
+
+
 
--- src/kmain.c
+++ src/kmain.c
@@ -3,19 +3,120 @@
 #include <stdio.h>
 #include <kernel/console.h>
 #include <kernel/timer.h>
+#include <kernel/vm.h>
+#include <kernel/hw.h>
 #include <multiboot.h>
 #define NULL 0
 #define PANIC(x) panic(__FILE__, __LINE__, x);
 void panic(char *file, int line,char* msg)
 {
-//	console_puts("PANIC:");
-//	console_puts(msg);
-//	console_puts("\n");
+	console_set_color(RED,WHITE);
 	console_puts("KERNEL PANIC\n");
 	printf("file:%s line:%i [%s]\n",file, line, msg);
 	while(1);
 }
-#define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
+void print_mb(unsigned long addr, unsigned long magic)
+{
+     #define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
+
+ /* Am I booted by a Multiboot-compliant boot loader? */
+       if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
+         {
+           printf ("Invalid magic number: 0x%x\n", (unsigned) magic);
+           return;
+         }
+           multiboot_info_t *mbi;
+ 
+       /* Set MBI to the address of the Multiboot information structure. */
+       mbi = (multiboot_info_t *) addr;
+     
+       /* Print out the flags. */
+       printf ("flags = 0x%x\n", (unsigned) mbi->flags);
+     
+       /* Are mem_* valid? */
+       if (CHECK_FLAG (mbi->flags, 0))
+         printf ("mem_lower = %uKB, mem_upper = %uKB\n",
+                 (unsigned) mbi->mem_lower, (unsigned) mbi->mem_upper);
+     
+       /* Is boot_device valid? */
+       if (CHECK_FLAG (mbi->flags, 1))
+         printf ("boot_device = 0x%x\n", (unsigned) mbi->boot_device);
+     
+       /* Is the command line passed? */
+       if (CHECK_FLAG (mbi->flags, 2))
+         printf ("cmdline = %s\n", (char *) mbi->cmdline);
+     
+       /* Are mods_* valid? */
+       if (CHECK_FLAG (mbi->flags, 3))
+         {
+           multiboot_module_t *mod;
+           unsigned int i;
+     
+           printf ("mods_count = %d, mods_addr = 0x%x\n",
+                   (int) mbi->mods_count, (int) mbi->mods_addr);
+           for (i = 0, mod = (multiboot_module_t *) mbi->mods_addr;
+                i < mbi->mods_count;
+                i++, mod++)
+             printf (" mod_start = 0x%x, mod_end = 0x%x, cmdline = %s\n",
+                     (unsigned) mod->mod_start,
+                     (unsigned) mod->mod_end,
+                     (char *) mod->cmdline);
+         }
+     
+       /* Bits 4 and 5 are mutually exclusive! */
+       if (CHECK_FLAG (mbi->flags, 4) && CHECK_FLAG (mbi->flags, 5))
+         {
+           printf ("Both bits 4 and 5 are set.\n");
+           return;
+         }
+     
+       /* Is the symbol table of a.out valid? */
+       if (CHECK_FLAG (mbi->flags, 4))
+         {
+           multiboot_aout_symbol_table_t *multiboot_aout_sym = &(mbi->u.aout_sym);
+     
+           printf ("multiboot_aout_symbol_table: tabsize = 0x%0x, "
+                   "strsize = 0x%x, addr = 0x%x\n",
+                   (unsigned) multiboot_aout_sym->tabsize,
+                   (unsigned) multiboot_aout_sym->strsize,
+                   (unsigned) multiboot_aout_sym->addr);
+         }
+     
+       /* Is the section header table of ELF valid? */
+       if (CHECK_FLAG (mbi->flags, 5))
+         {
+           multiboot_elf_section_header_table_t *multiboot_elf_sec = &(mbi->u.elf_sec);
+     
+           printf ("multiboot_elf_sec: num = %u, size = 0x%x,"
+                   " addr = 0x%x, shndx = 0x%x\n",
+                   (unsigned) multiboot_elf_sec->num, (unsigned) multiboot_elf_sec->size,
+                   (unsigned) multiboot_elf_sec->addr, (unsigned) multiboot_elf_sec->shndx);
+         }
+      /* Are mmap_* valid? */
+       if (CHECK_FLAG (mbi->flags, 6))
+         {
+           multiboot_memory_map_t *mmap;
+     
+           printf ("mmap_addr = 0x%x, mmap_length = 0x%x\n",
+                   (unsigned) mbi->mmap_addr, (unsigned) mbi->mmap_length);
+           for (mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
+                (unsigned long) mmap < mbi->mmap_addr + mbi->mmap_length;
+                mmap = (multiboot_memory_map_t *) ((unsigned long) mmap
+                                         + mmap->size + sizeof (mmap->size)))
+             printf (" size = 0x%x, base_addr = 0x%x%x,"
+                     " length = 0x%x%x, type = 0x%x\n",
+                     (unsigned) mmap->size,
+                     (unsigned) mmap->base_addr_high,
+                     (unsigned) mmap->base_addr_low,
+                     (unsigned) mmap->length_high,
+                     (unsigned) mmap->length_low,
+                     (unsigned) mmap->type);
+         } 
+
+}
+
+
+//#define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
 void kmain(uint32_t mbd, uint32_t magic)
 {
 	struct multiboot_info *mb;
@@ -28,22 +129,23 @@ void kmain(uint32_t mbd, uint32_t magic)
 		return;
    	}
  	mb = (struct multiboot_info *)mbd;
-	mb = mb;	
-	/* begin initializations */	
+
+
+	/* begin initializations */
+	paging_init();	
 	interrupt_init();
 	console_init();
-
-
-
-//	if (CHECK_FLAG (mb->flags, 2))
-  //  	printf ("cmdline = %s\n", (char *) mb->cmdline);
+	console_set_color(BLUE,WHITE);
+	vm_init();
+	printf("Modules %i\n", mb->mods_count);
+	void *ptr = *(void **)mb->mods_addr; 
+	puts((char *)ptr);
+	puts("\n");
+	printf("addr %x\n", *(void **)(mb->mods_addr + 4));
+	printf("addr %x\n", *(void **)(mb->mods_addr));
+	console_set_color(BLACK,WHITE);
 	
-//	for(int i = 0; i < 43; i++)
-	{
-		printf("hello, world \n");
-//		printf("hello, world %i\n",i);
-//		printf("hello, world %x\n",i);
-	}
-//asm volatile ("int $0x1");
+	printf("hello, world \n");
+	
 	PANIC("kmain returned");
 }
