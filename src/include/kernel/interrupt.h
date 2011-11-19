2e4d290ee585e45a96f6232cfa912a6702ab1a15
Bryce Lanham <blanham@gmail.com>
2011-11-18 17:50:01 -0800

    added header guard to interrupt.h

--- src/include/kernel/interrupt.h
+++ src/include/kernel/interrupt.h
@@ -1,3 +1,6 @@
+#ifndef C_OS_INTERRUPT_H
+#define C_OS_INTERRUPT_H
+
 #include <kernel/types.h>
 enum {
 	IRQ0 = 32, IRQ1, IRQ2, IRQ3,
@@ -15,10 +18,11 @@ struct registers
 
 typedef struct registers registers_t;
 
+void dump_regs(struct registers *regs);
 void interrupt_init();
 
 typedef void intr_handler (struct registers *);
 void interrupt_register(int irq, intr_handler *handler);
 
 enum { PTI_I, KBD_I = IRQ1, CASC_I, COM2_I, COM1_I, FD_I};
-
+#endif
