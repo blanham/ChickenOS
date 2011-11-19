fce97ef88400cf8d7cd997e052d36382abd99d68
Bryce Lanham <blanham@gmail.com>
2011-11-18 17:47:43 -0800

    removed extraneous size_t declaration

--- src/include/kernel/types.h
+++ src/include/kernel/types.h
@@ -9,5 +9,7 @@ typedef signed   short 	int16_t;
 typedef unsigned short 	uint16_t;
 typedef signed   int 	int32_t;
 typedef unsigned int 	uint32_t;
-typedef uint32_t		size_t;
+typedef uint32_t	    bool;
+#define FALSE 0
+#define TRUE 1
 #endif
