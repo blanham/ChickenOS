05e6e71c132b5776722ef4ede2d30fe07bafbc33
Bryce Lanham <blanham@gmail.com>
2011-11-23 00:19:40 -0600

    wrote basic page allocator

--- src/include/kernel/vm.h
+++ src/include/kernel/vm.h
@@ -1,7 +1,13 @@
+#ifndef C_OS_VM_H
+#define C_OS_VM_H
+#include <kernel/types.h>
 #define PHYS_BASE 0xC0000000
 #define V2P(p) (p - PHYS_BASE)
 #define P2V(p) (p + PHYS_BASE)
 
+extern unsigned int placement;
+
 void paging_init();
 void gdt_install();
-void vm_init();
+void vm_init(uint32_t);
+#endif
