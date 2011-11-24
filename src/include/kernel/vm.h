542555205a714f54257a03abdefe6ea9ab688fe0
Bryce Lanham <blanham@gmail.com>
2011-11-24 11:20:42 -0600

    added palloc|n prototypes to vm.h, add function to make a new page directory by copying the kernel pagedir, which now has a kernel_pd pointer, also defines pagedir_t

--- src/include/kernel/vm.h
+++ src/include/kernel/vm.h
@@ -4,10 +4,18 @@
 #define PHYS_BASE 0xC0000000
 #define V2P(p) (p - PHYS_BASE)
 #define P2V(p) (p + PHYS_BASE)
+typedef uint32_t phys_addr_t;
+typedef uint32_t virt_addr_t;
+typedef uint32_t * pagedir_t;
+#define NULL (void *)0
 
 extern unsigned int placement;
 
 void paging_init();
 void gdt_install();
 void vm_init(uint32_t);
+void *pallocn(uint32_t count);
+void *palloc();
+
+pagedir_t pagedir_new();
 #endif
