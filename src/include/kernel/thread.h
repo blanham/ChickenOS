ac804c1065070e2963f2a456dbf838ebd8e174d5
Bryce Lanham <blanham@gmail.com>
2011-11-24 11:21:06 -0600

    more work on thread_create

--- src/include/kernel/thread.h
+++ src/include/kernel/thread.h
@@ -1,12 +1,14 @@
 #ifndef C_OS_THREAD_H
 #define C_OS_THREAD_H
 #include <kernel/list.h>
+#include <kernel/vm.h>
 typedef unsigned short pid_t;
 typedef struct thread {
 	pid_t pid;
 	pid_t parent;
 	struct list_head list;
-
+	uint8_t *sp;
+	pagedir_t pd;	
 } thread_t;
 
 void thread_init();
