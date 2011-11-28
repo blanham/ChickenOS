62a7fd90a8318cad9de3e09ea821d796dd4426f7
Bryce Lanham <blanham@gmail.com>
2011-11-27 21:49:33 -0600

    more work on context switching, almost works

--- src/thread.c
+++ src/thread.c
@@ -16,12 +16,42 @@ LIST_HEAD(all_list);
 #define STACK_PAGES STACK_SIZE/PAGE_SIZE
 extern uint32_t stack;
 
+typedef struct {
+	uint32_t old_tss;
+	uint32_t esp0;
+	uint32_t ss0;
+	uint32_t esp1;
+	uint32_t ss1;
+	uint32_t esp2;
+	uint32_t ss2;
+	uint32_t cr3;
+	uint32_t eip;
+	uint32_t eflags;
+	uint32_t eax;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t ebx;
+	uint32_t esp;
+	uint32_t ebp;
+	uint32_t esi;
+	uint32_t edi;
+	uint32_t es;
+	uint32_t cs;
+	uint32_t ss;
+	uint32_t ds;
+	uint32_t fs;
+	uint32_t gs;
+	uint32_t ldt;
+	uint32_t io_bmap;
+} tss_t;
 
+tss_t tss;
 
 pid_t pid_allocate();
 
 void thread_init()
 {
+	asm volatile("cli");
 	kernel_stack_bottom = (void *)&stack;
 //	printf("stack bottom %x\n", kernel_stack_bottom);
 
@@ -31,13 +61,16 @@ void thread_init()
 	
 //	printf("esp %x\n",esp);// & ~0x3FFF);
 	kernel_thread->pid = 0;
+	kernel_thread->parent = 12345;
 //	kernel_thread->sp = (uint8_t  *)esp;//(uint8_t *)kernel_thread + STACK_SIZE;
 	
-
+	kernel_thread->sp = (uint8_t *)0xDEADBEEF;
 //	all_list = &kernel_thread->list;
-//	kernel_thread->list = INIT_LIST_HEAD(&all_list.list);
-//	LIST_HEAD(&(kernel_thread->list));
-	list_add(&kernel_thread->list, &all_list);
+	INIT_LIST_HEAD(&kernel_thread->list);
+	//LIST_HEAD(&(kernel_thread->list));
+//	list_add(&kernel_thread->list, &all_list);
+
+	asm volatile( "sti");
 //	INIT_LIST_HEAD(&all_list->list);
 }
 
@@ -60,53 +93,60 @@ void jump()
 }
 int test = 0;
 thread_t *test_thread = 0;
+
+
+
 void thread_scheduler(struct registers *regs)
 {
+
+
 	regs = regs;
-	thread_t *cur = thread_current();//?
- cur = cur;
-//	struct list_head *head = &cur->list;
-	uint32_t _esp;
-	asm ("mov %%esp, %0": "=r"(_esp) );
-//	printf("esp sched %X\n",_esp);
-	thread_t *next;
-	struct list_head *pos = cur->list.next;
-//	list_for_each_entry(next, all_list, list)
-//	list_for_each(pos, &all_list)
-//	{
-		//later check if thread is ready
-		next = list_entry(pos, thread_t, list);
-//		if(next == cur) continue;	
-	//	printf("pid %i\n", next->pid);
-		if(test_thread == 0)
-			goto aer;
-		else
-			next = test_thread;
-//	}
-////	if(next->pid == 0)// == kernel_thread)
-//		goto aer; 
-	//extern void intr_return();
-	printf("PID= %i\n", next->pid);
-	printf("Size %i\n",sizeof(registers_t));
-//	_esp -= 76 + 8;//0x30 -i 4;
-	printf("test %X\n", _esp + 0x30);
-	//	_esp = (uint32_t)next->sp + 56;//56;
-	_esp = (uint32_t)next->sp;
-	printf("THREAD ESP %X\n",_esp);
-	asm volatile ("mov %0, %%esp\n" :: "r"(_esp));
-	asm volatile("cli");
+	uint32_t _esp = 0xdeadbeef;
+	thread_t *cur = thread_current();
+	thread_t *next = 0;
+	uint32_t esp_old = 0xdeadbeef;
+
+//	uint32_t TEST = 0;
+//	TEST = TEST;
+	asm volatile ("mov %%esp, %0\n" : "=r"(esp_old));
+
+
 	
-	asm volatile("iret\n");
-//	asm volatile ("jmp intr_return");
-/*	asm volatile ("mov %0,%%esp\n"
+
+
+		next = list_entry(&cur->list.next, thread_t, list);
+	//	printf("PID = %i\n",next->pid);
+	
+
+	thread_t *tes = list_entry(&next->list.next, thread_t, list);
+	printf("%X %X %X\n", next, tes, cur);
+		printf("PID = %i\n",tes->pid);
+//	if(cur->pid == 1)
+	//	while(1);
+//	printf("lulx next->sp %X\n", next->sp);
+
+//	if(next->sp == 0)
+//		next->sp = (uint8_t *)(0xc0110f4c + 13*4);
+	esp_old = regs->esp - 0x24;
+ 	printf("%x %x\n", esp_old, regs->esp - 0x20 + 3*4);
+	cur->sp = (uint8_t *)(esp_old + 0);
+
+	_esp = (uint32_t)next->sp;// + 56;
+//	printf("%x %x\n", next->sp, cur->sp);
+//	if(next != cur) while(1);
+//	dump_regs((registers_t *)next->sp);
+	//return;
+//	printf("SCHED ESP %X ESPOLD %X\n",_esp, esp_old);
+	
+//	extern void pic_send_end(int);
+//		pic_send_end(0x20);
+//	if(next->pid == 0)
+	//while(1);
+//	asm volatile("sti");
+	asm volatile ("mov %0,%%esp\n"
 					"jmp intr_return"
-					: "=m"(next->sp)
+					:: "m"(_esp)
 				);
-*/
-aer:
-	;
-//	printf("switch\n");
-//	context_switch()
 
 
 }
@@ -148,7 +188,6 @@ thread_t * thread_create(void (*func)(void *), void *aux)
 	aux = aux;
 
 //add new to list
-	list_add(&new->list,&all_list);
 	//list_add_tail(&(new->list),&all_list.list);
 //set new to run
 	//will set status to run here
@@ -157,18 +196,26 @@ thread_t * thread_create(void (*func)(void *), void *aux)
 //uint8_t *new_ksp = palloc()
 //asm volatile (" " );
 //	new_sp = new_sp - sizeof(registers_t);
-	new_sp -= sizeof(struct tregs);	
+	printf("INTR FRAME %X\n",new_sp - sizeof(struct tregs));
+	new_sp -= sizeof(struct registers);	
 //	struct registers *reg_frame = (struct registers *)new_sp;
-	struct tregs *reg_frame= (struct tregs *)new_sp;
+	struct registers *reg_frame= (struct registers *)new_sp;
 	reg_frame->eip = (uint32_t)new_eip;
 	printf("NEW EIP %X\n",new_eip);
 	printf("EIP %x\n",reg_frame->eip);
 	reg_frame->useresp = (uint32_t)new_sp;
 //	reg_frame->esp = (uint32_t)new_sp;
-	reg_frame->cs = reg_frame->ss = 0x10;
+	uint32_t _cs, _ss;
+		
+	asm volatile ("mov %%cs, %0\n" : "=r"(_cs));
+	asm volatile ("mov %%ss, %0\n" : "=r"(_ss));
+	reg_frame->cs = _cs;
+ 	reg_frame->ss = _ss;
 test = 1;
 	new->sp = new_sp;
 	printf("new_esp = %x\n",new_sp);
+	
+	list_add(&new->list,&all_list);
 	asm volatile("sti");
 	test_thread = new;
 	return new;
