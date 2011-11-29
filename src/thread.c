eb933b78b5ccf83b8e55fc1aa24dfa0597a3fc85
Bryce Lanham <blanham@gmail.com>
2011-11-28 20:02:10 -0600

    multitasking now works, can create threads, forking not yet implemented

--- src/thread.c
+++ src/thread.c
@@ -1,20 +1,18 @@
 #include <kernel/types.h>
 #include <kernel/thread.h>
+#include <kernel/memory.h>
 #include <kernel/interrupt.h>
-#include <stdio.h>
+#include "debug.h"
 
 void *kernel_stack_bottom;
-
+extern uint32_t stack;
 thread_t *kernel_thread;
 
-//thread_t *ready_list;
-//thread_t *all_list;
-//thread_t all_list;
 LIST_HEAD(all_list);
+
 #define PAGE_SIZE 0x1000
 #define STACK_SIZE 0x4000
 #define STACK_PAGES STACK_SIZE/PAGE_SIZE
-extern uint32_t stack;
 
 typedef struct {
 	uint32_t old_tss;
@@ -53,200 +51,242 @@ void thread_init()
 {
 	asm volatile("cli");
 	kernel_stack_bottom = (void *)&stack;
-//	printf("stack bottom %x\n", kernel_stack_bottom);
 
-	kernel_thread = kernel_stack_bottom;
-	uint32_t esp;
-	asm ("mov %%esp, %0": "=m"(esp) );
+	kernel_thread = thread_current();//kernel_stack_bottom;
 	
-//	printf("esp %x\n",esp);// & ~0x3FFF);
 	kernel_thread->pid = 0;
-	kernel_thread->parent = 12345;
-//	kernel_thread->sp = (uint8_t  *)esp;//(uint8_t *)kernel_thread + STACK_SIZE;
-	
+	kernel_thread->parent = 0;
 	kernel_thread->sp = (uint8_t *)0xDEADBEEF;
-//	all_list = &kernel_thread->list;
-//	INIT_LIST_HEAD(&kernel_thread->list);
-	//LIST_HEAD(&(kernel_thread->list));
-	list_add(&kernel_thread->list, &all_list);
 
-	asm volatile( "sti");
-//	INIT_LIST_HEAD(&all_list->list);
+	kernel_thread->status = RUNNING;
+
+	INIT_LIST_HEAD(&kernel_thread->list);
+
 }
 
 thread_t * thread_current()
 {
 
-	uint32_t _esp;
-	asm ("mov %%esp, %0": "=m"(_esp) );
-//	printf("esp %x\n",_esp);// & ~0x3FFF);
+	thread_t *tmp; 
+	asm ("mov %%esp, %0": "=m"(tmp) );
 
-	thread_t *tmp = (thread_t *) (_esp & ~0x3FFF);
-
-	return tmp;
-}
-void jump()
-{
-	printf("jumped here\n");
-	while(1);
+	return (thread_t *) ((uint32_t)tmp & ~0x3FFF);
 
 }
-int test = 0;
-thread_t *test_thread = 0;
-
-int test2 = 0;
 
 void thread_scheduler(struct registers *regs)
 {
-
-
-	regs = regs;
 	uint32_t _esp = 0xdeadbeef;
 	thread_t *cur = thread_current();
-	thread_t *next = 0;
-	uint32_t esp_old = 0xdeadbeef;
-
-//	uint32_t TEST = 0;
-//	TEST = TEST;
-	//asm volatile ("mov %%esp, %0\n" : "=r"(esp_old));
-
-thread_t *tt;
-struct list_head *pos;
-list_for_each(pos, &all_list)	
-{
-	tt = list_entry(pos, thread_t,list);
-//	printf("PID &%X\n", tt->pid);
-	if(tt->pid != cur->pid)
-		break;
-
-}
-	if(cur != kernel_thread)
-		tt = kernel_thread;
-	else if(test)
-		tt = test_thread;
-//		next = list_entry(&cur->list.next, thread_t, list);
-	//	printf("PID = %i\n",next->pid);
+	thread_t *next = list_entry(cur->list.next, thread_t, list);
 	
-
-//	thread_t *tes = list_entry(&next->list.next, thread_t, list);
-//	printf("%X %X %X\n", next, tes, cur);
-	//next = tes;
-	next = tt;
-//		printf("PID = %i\n",next->pid);
-//	if(cur->pid == 1)
-	//	while(1);
-//	printf("lulx next->sp %X\n", next->sp);
-
-//	if(next->sp == 0)
-//		next->sp = (uint8_t *)(0xc0110f4c + 13*4);
-	esp_old = regs->esp - 0x24;
-// 	printf("%x %x\n", esp_old, regs->esp - 0x24);
-	cur->sp = (uint8_t *)(esp_old + 0);
-
-	_esp = (uint32_t)next->sp;// + 56;
-//	printf("%x %x\n", next->sp, cur->sp);
-//	if(next != cur) while(1);
-//	dump_regs((registers_t *)next->sp);
-	//return;
-//	printf("SCHED ESP %X ESPOLD %X\n",_esp, esp_old);
+	cur->sp = (uint8_t *)regs->ESP;
+	_esp = (uint32_t)next->sp;
 	
-//	extern void pic_send_end(int);
-//		pic_send_end(0x20);
-	//if(test2 == 1)
-	//while(1);
-	//if(next->pid == 1)
-	//	goto new;
-//	asm volatile("sti");
-	//if(next->pid == 1)
-	//;//	while(1);
 	asm volatile ("mov %0,%%esp\n"
 					"jmp intr_return"
 					:: "m"(_esp)
 				);
+}
 
+/* throw an int 32, manually invoking the timer interrupt */
+void thread_yield()
+{
+	asm volatile("int $32");
+}
+
+
+void thread_exit()
+{
+	asm volatile("cli");
+	thread_t *cur = thread_current();
+	//keep a tmp pointer to next process
+	//which the scheduler uses to get the next process
+	struct list_head *tmp = cur->list.next;
+	list_del(&cur->list);
+	cur->list.next = tmp;
+	//if we aren't the original kernel thread, free the stack
+	if(cur->pid == 0)
+	{
+		pallocn_free(cur, STACK_PAGES);	
+
+	}
+	asm volatile("sti");
+	thread_yield();
 
 }
+extern uint32_t get_eip();
+/* need to clone page directory properly first */
 pid_t fork()
 {
+	return -1;
+	/* this fixe interrupt in created threads, needs to be fixed */
+/*	uint32_t _eflags;
+	asm volatile ("pushfl\n" "pop %0\n" : "=r"(_eflags)::"esp");
+	
+	
+	asm volatile ("cli");
+	
+	thread_t *new = pallocn(STACK_PAGES);
+	thread_t *cur = thread_current();
+//	uint32_t *args;
+	uint32_t new_sp, cur_sp;
+	uint32_t new_bp = 0, cur_bp;
+	uint32_t top_sp;
+	struct registers *reg_frame;
+	int i = 1;
+	while((int)new % STACK_SIZE)
+	{
+		
+		pallocn_free(new, STACK_PAGES);
+		new = pallocn(i);
+		i++;	
+	}
+	
+	kmemset((uint8_t *)new, 0, STACK_SIZE);
 
+	//printf("thread create\n");	
 
+	if(new == NULL)
+		return -1;
 
-	return 0;
+	new->pd = pagedir_new();	
+	
+	new_sp = (uint32_t)((uint32_t)new + STACK_SIZE);
+
+	kmemcpy((uint8_t *)new,(uint8_t*)cur, STACK_SIZE);
+
+	//args = (uint32_t *)(new_sp - 4);
+	
+	asm ("mov %%esp, %0": "=m"(cur_sp));
+	asm ("mov %%ebp, %0": "=m"(cur_bp));
+	top_sp = ((uint32_t)cur_sp & (uint32_t)0x3FFF);// + STACK_SIZE;
+	uint32_t offset = top_sp;// - cur_sp;
+	new_bp = (cur_bp & 0x3fff);
+
+	uint32_t eip = get_eip();
+	printf("THREAD %X\n", thread_current());
+	if(thread_current() == cur)
+	{
+		new->pid = pid_allocate();
+		new->parent = cur->pid;
+
+		new_sp -= offset - 64;
+		uint32_t tmp = new_sp;
+		new_bp = new_sp - new_bp;
+		printf("%X %X %X %X\n",tmp, new_sp, cur_sp, cur_bp);
+		new_sp -= sizeof(struct registers);	
+		reg_frame= (struct registers *)new_sp;
+		new->regs = (struct registers *)new_sp;
+		reg_frame->eip = (uint32_t)eip;
+	
+		uint32_t _cs, _ss, _ds;
+		asm volatile ("mov %%cs, %0\n" : "=r"(_cs));
+		asm volatile ("mov %%ds, %0\n" : "=r"(_ds));
+		asm volatile ("mov %%ss, %0\n" : "=r"(_ss));
+		reg_frame->cs = _cs;
+ 		reg_frame->ss = _ss;
+ 		reg_frame->ds = _ds;
+		reg_frame->eflags = _eflags;
+	
+		reg_frame->esp = (uint32_t)tmp;
+
+		reg_frame->ebp = (uint32_t)new_bp;	
+		reg_frame->useresp = (uint32_t)tmp;
+
+		new->sp = (uint8_t *)new_sp - 4;
+
+		list_add_tail(&new->all_list,&all_list);
+		list_add_tail(&new->list,&kernel_thread->list);
+		printf("DUMP\n");
+		dump_regs(reg_frame);
+		
+		printf("HERE\n");	
+		asm volatile("sti");
+		return new->pid;
+	}else{
+	//	PANIC("FORKED\n");
+
+	//	while(1);
+		return 0;
+	}
+	// this has to be set here, otherwise trashed 
+	// *args = (uint32_t)aux;
+	
+	//asm volatile("sti");
+	
+	return 0;*/
+	
 }
-void dummy()
-{
 
-	printf("hello from dummy\n");
-
-}
-struct tregs
-{
-   uint32_t eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
-} __attribute__((packed));
-
-
-	char *derp = "test";
 thread_t * thread_create(void (*func)(void *), void *aux)
 {
-//allocate pagest
-	asm volatile ("cli");
-	thread_t *new = pallocn(STACK_PAGES);
-	printf("thread create\n");	
+	/* this fixes interrupt in created threads, needs to be fixed */
+	uint32_t _eflags;
+	asm volatile ("pushfl\n" "pop %0\n" : "=r"(_eflags)::"esp");
 	
+	asm volatile ("cli");
+	
+	thread_t *new = pallocn(STACK_PAGES);
+	uint32_t *args;
+	uint32_t new_sp;
+	struct registers *reg_frame;
+	/* need a function to return aligned palloc requests */
+	int i = 1;
+	while((int)new % STACK_SIZE)
+	{
+		
+		pallocn_free(new, STACK_PAGES);
+		new = pallocn(i);
+		i++;	
+	}
+	
+	kmemset((uint8_t *)new, 0, STACK_SIZE);
+
 	if(new == NULL)
 		return NULL;
 
 	new->pd = pagedir_new();	
-	printf("new %x\n", new);
-	uint32_t new_sp = (uint32_t)(new + STACK_SIZE);
-	new->pid = 1;//pid_allocate();
+	
+	new_sp = (uint32_t)((uint32_t)new + STACK_SIZE);
+	args = (uint32_t *)(new_sp - 4);
+	
+	new->pid = pid_allocate();
 	new->parent = thread_current()->pid;
-	void *new_eip = func;
-	aux = derp;
-
-//add new to list
-	//list_add_tail(&(new->list),&all_list.list);
-//set new to run
-	//will set status to run here
-//setup stack
-
-//uint8_t *new_ksp = palloc()
-//asm volatile (" " );
-//	new_sp = new_sp - sizeof(registers_t);
-	printf("NEW_SP %X\n", new_sp);
-	uint32_t saved_sp = new_sp;	
-
-	printf("INTR FRAME %X\n",new_sp - sizeof(struct registers));
+	
 	new_sp -= sizeof(struct registers);	
-	printf("NEW_SP %X\n", new_sp);
-//	struct registers *reg_frame = (struct registers *)new_sp;
-	struct registers *reg_frame= (struct registers *)new_sp;
-	reg_frame->eip = (uint32_t)new_eip;
-	printf("NEW EIP %X\n",new_eip);
-	printf("EIP %x\n",reg_frame->eip);
-//	reg_frame->esp = (uint32_t)new_sp;
-	uint32_t _cs, _ss;
-		
+	reg_frame= (struct registers *)new_sp;
+	new->regs = (struct registers *)new_sp;
+	reg_frame->eip = (uint32_t)func;
+	
+	uint32_t _cs, _ss, _ds;
 	asm volatile ("mov %%cs, %0\n" : "=r"(_cs));
+	asm volatile ("mov %%ds, %0\n" : "=r"(_ds));
 	asm volatile ("mov %%ss, %0\n" : "=r"(_ss));
 	reg_frame->cs = _cs;
  	reg_frame->ss = _ss;
-test = 1;
-/*	new_sp = (uint8_t *)aux;
-		
-
-	new_sp--;
-*/	printf("new_esp = %x\n",new_sp);
+ 	reg_frame->ds = _ds;
+	reg_frame->eflags = _eflags;
 	
-	reg_frame->useresp = (uint32_t)saved_sp;
-	reg_frame->esp = (uint32_t)saved_sp;
+	
+	reg_frame->useresp = (uint32_t)new_sp;
+
 	new->sp = (uint8_t *)new_sp - 4;
-//	list_add_tail(&new->list,&kernel_thread->list);
-	list_add_tail(&new->list,&all_list);
-	//while(1);
-	test_thread = new;
+
+	list_add_tail(&new->all_list,&all_list);
+	list_add_tail(&new->list,&kernel_thread->list);
+
+	/* this has to be set here, otherwise trashed */
+	*args = (uint32_t)aux;
+	
 	asm volatile("sti");
+	
 	return new;
+}
+
+pid_t get_pid()
+{
+	return thread_current()->pid;
 }
 
 pid_t pid_allocate()
@@ -256,7 +296,6 @@ pid_t pid_allocate()
 	pid_count++;
 
 	return pid_count;
-	
 }
 /*
 
