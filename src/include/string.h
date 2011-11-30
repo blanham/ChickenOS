deb3f534fdef7d1db78dab9fff3b9d50f54a870e
Bryce Lanham <blanham@gmail.com>
2011-11-30 15:18:15 -0600

    added strcmp function

--- src/include/string.h
+++ src/include/string.h
@@ -6,4 +6,5 @@ int strlen(char *str);
 void *memcpy(void *, const void *, size_t);
 
 
+int strcmp (const char * str1, const char * str2);
 #endif
--- src/lib/string.c
+++ src/lib/string.c
@@ -6,7 +6,16 @@ int strlen(char *str)
 		tmp++;
 	return tmp - str;
 }
+int strcmp ( const char * str1, const char * str2 )
+{
+	int i = 0;	
 
+	for(; str1[i] == str2[i]; i++)
+		if(str1[i] == '\0')
+			return 0;	
+
+	return str1[i] - str2[i]; 
+}
 void *memcpy(void *dst, const void *src, size_t size)
 {
 	char *_dst = dst;
