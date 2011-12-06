b8cf7b10b654ec42d55a7c9899b15dd6feda2383
Bryce Lanham <blanham@gmail.com>
2011-12-06 17:36:07 -0600

    first draft of vfs support, character and block devices are implemented, and part of the initrd ramdisk device is implemented

--- src/fs/vfs.c
+++ src/fs/vfs.c
@@ -1,106 +1,348 @@
 //#include "include/kernel/types.h"
-//#include <kernel>
+#include <kernel/types.h>
+#include <kernel/memory.h>
+#include <kernel/thread.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include "vfs.h"
-#include "ext2fs.h"
+//#include "ext2fs.h"
+#include "initrd.h"
 #define UNUSED(X) X = X
+vfs_fs_t *main_fs;
+uint8_t file_count = 0;
+struct file files[100];
+
+
+struct file *open_files[100];
+/*
+int vfs_read_blocknum(vfs_fs_t *fs, uint8_t *dst, uint32_t blocknum)
+{
+
+	return i-1;
+
+}*/
+struct file root_storage;
+struct file *root = &root_storage;
+
 //wrap malloc(), so we can later call c_os's kernel allocator
 void *vfs_malloc(size_t size)
 {
-	return malloc(size);
+	UNUSED(size);
+	//return malloc(size);
+	return NULL;
 }
 
-typedef struct vfs_superblock_ops {
-	vfs_read_inode_t read;
-	vfs_write_inode_t write;
-} vfs_superblock_ops_t;
-typedef struct vfs_superblock {
-	vfs_superblock_ops_t;
-} vfs_superblock_t;
 
 
+struct file *file_alloc()
+{
+	struct file *ret = &files[file_count];
+	file_count++;
+	if(file_count > 127)
+		return NULL;
+	else
+		return ret;
+}
 
 
-typedef struct vfs_fs {
-	vfs_superblock_t superblock;
+int vfs_register_fs(vfs_fs_t *fs)
+{
+	if(fs == NULL)
+		return -1;
+	main_fs = fs;
 
-} vfs_fs_t;
+	return 0;
+}
 
-typedef struct inode_cached {
-	uint32_t group;
-	uint32_t inode;
+struct file *dir_search(struct file *dir, char *name)
+{
+	struct file *elem = dir->dir;
+	if(elem == NULL)
+		return NULL;
+	if(!strncmp(dir->name, name, 256))
+		return dir;
 
+	for(;elem->file != NULL; elem = elem->file)
+	{	
+		if(!strncmp(elem->name, name, 256))
+			return elem;
+	}
+	return elem;
+}
 
+struct file * pathsearch(struct file *dir, char *_path)
+{
+	//UNUSED(vfs);
+	UNUSED(_path);
+	char *saveptr;
+	//if dir is not a directory, we need to throw an error
+	//char *path = _path;
+//	char *path = malloc(500);
+	char path[500];
+	strcpy(path, _path);
+	char *token = (char *)strtok_r(path, "/", &saveptr);
+	if( token == NULL)
+		return NULL;
+	struct file *elem = dir_search(dir, token);
+	if(elem == NULL){
+		printf("dir search or insert broken\n");
+		return NULL;
+	}
+	while((token = (char *)strtok_r(NULL, "/", &saveptr)) != NULL)
+	{
+		elem = dir_search(elem, token);
+		if(elem == NULL)
+		{
+			printf("file not found?\n");
+			return NULL;
+		}
+	}
 
-} inode_cached_t;
-
-
-
-
-
-typedef struct vfs_file {
-	void *filesystem;
-	uint32_t inode;
-	char *name;//perhaps use name[255/256] instead?NO! what about other fses?
 	
+
+	return elem;
+}
+
+int insert_file(struct file *dir, struct file *file)
+{
+	struct file *elem = dir->dir;
+	
+	if(elem == NULL)
+	{
+		dir->dir = file;
+		file->parent = dir;
+		return 0;
+	}
+	
+	for(;elem->file != NULL; elem = elem->file);
+		elem->file = file;
+	elem->file->parent = dir;	
+	return 0;
+}
+
+
+
+struct file *file_new(char *name)
+{
+	struct file *new = file_alloc();//malloc(sizeof(struct file));
+	kmemset((uint8_t *)new,0, sizeof(*new));
+	strcpy(new->name, name);
+	return new;
+}
+
+struct file *file_new3(char *name, uint8_t type, uint16_t device)
+{
+	struct file *new = file_alloc();//malloc(sizeof(struct file));
+	kmemset((uint8_t *)new,0, sizeof(*new));
+	strcpy(new->name, name);
+	new->device = device;
+	new->type = type;
+	return new;
+}
+
+
+struct file *file_new2(vfs_fs_t *fs, char *name, uint32_t inode)
+{
+	struct file *new = file_alloc();//malloc(sizeof(struct file));
+	kmemset((uint8_t *)new,0, sizeof(*new));
+	strcpy(new->name, name);
+	new->inode = inode;
+	new->fs = fs;
+	return new;
+}
+
+
+
+
+struct file * dir_new(struct file *dir, char *name)
+{
+	struct file *dot = file_new(".");
+	struct file *dotdot = file_new("..");
+	struct file *new = file_new(name);
+	insert_file(dir, new);	
 	
 
-} vfs_file_t;
+	insert_file(new, dot);
+	insert_file(new, dotdot);
+	dot->dir = new->dir;
+	dotdot->dir = new->parent->dir;
+	
+	return new;
 
-struct file;
-struct file {
-	struct file *parent;
-	//left
-	struct file *dir;
-	//right
-	struct file *file;
-	struct vfs_file *vfs;	
-};
+}
+void dir_list(struct file *dir)
+{
+	struct file * elem = dir->dir;
+	if(dir == NULL)
+	{
+		printf("passed NULL dir to dir_list\n");
+		return;
+	}
+	if(elem == NULL)
+	{
+		printf("File %s is not a directory\n", dir->name);
+		return;
+	}
+	printf("Directory listing for %s\n",elem->parent->name);
+	for(;elem != NULL; elem = elem->file)
+		printf("%s\n",elem->name);
 
-struct file root;
+}
 
 void vfs_init()
 {
-	root.parent = &root;
-	root.dir = NULL;
-	root.file = NULL;
+	strcpy(root->name,"/");
+	root->parent = root;
+	struct file *dot = file_new(".");
+	struct file *dotdot = file_new("..");
+	printf("Initialzing VFS\n");
+//	printf("dotdot %x %x\n", (uintptr_t)dotdot->dir, (uintptr_t)root->dir);	
+	
+	insert_file(root, dot);
+	insert_file(root, dotdot);
+	struct file *test = file_new("test");
+	insert_file(root, test);
+	dot->dir = root->dir;
+	dotdot->dir = root->dir;
+	struct file * dir = dir_new(root, "foo");
+	dir_new(dir, "dev");
 
-
+	thread_current()->cur_dir = root; 
+//	dir_list(root);
+//	dir_list(dir);
 }
-int pathsearch(char *path)
+
+
+#include <stdio.h>
+struct file * vfs_root()
 {
 
 
-
+	return root;
+}
+uint8_t *ramdisk;
+void vfs_mount(vfs_fs_t *fs, struct file *dir)
+{
+	UNUSED(fs);
+	UNUSED(dir);
+	
 
 
 }
 
+vfs_fs_t *root_fs;
+int old_main(int argc, char **argv)
+{
+	UNUSED(argc);
+	UNUSED(argv);
+	
 
+	if(argc < 2)
+		return -1;
+		
+//	ext2_init(root_fs,argv[1]);
+//	ext2(argv[1]);
+/*	return 0;
+	FILE *fp = fopen(argv[1], "rb");
+	fseek(fp, 0, SEEK_END);
+	size_t size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	printf("SIZE %x\n",(uint32_t)size);
+	ramdisk = malloc(size);
+	fread(ramdisk, size, 1, fp);
+	if(ramdisk == NULL)
+		printf("fuck\n");
+	initrd_init((uintptr_t)ramdisk, (uintptr_t)ramdisk + (uintptr_t)size);
+	fclose(fp);
+
+	uint8_t *first_sector = malloc(SECTOR_SIZE);
+	if(initrd_read_block(first_sector, 0) < 0)
+		return -1;
+	fp = fopen("out","wb");
+	fwrite(first_sector, SECTOR_SIZE,1,fp);
+	fclose(fp);
+	printf("%s\n",first_sector+3);
+//	uint16_t *test = (uint16_t *)first_sector;
+	printf("test %.2x%.2x\n", first_sector[510],first_sector[511]);
+*/
+	return 0;
+}
 /*int open(const char *path, int oflag, ...)*/
+int fd_new()
+{
+	static int fd = 3;
+	
+
+	return fd++;
+
+
+}
+
 int open(const char *path, int oflag)
 {
 	UNUSED(path);
 	UNUSED(oflag);
-	return -1;
+	struct file *dir = thread_current()->cur_dir;
+	struct file * fp = pathsearch(dir, (char *)path);
+	if(fp == NULL)
+		return -1;
+	int fd = fd_new();
+	open_files[fd] = fp;
+
+	return fd;
 }
+
+size_t vfs_read(struct file *file, void *buf, size_t nbyte)
+{
+	int ret;
+
+	if((file->type & (FILE_CHAR | FILE_BLOCK)) != 0){
+		printf("REAFDASDF\n");
+		ret = device_read(file, buf, file->offset,nbyte);
+		printf("ASDFASDF");
+	}else{
+		if(file->fs == NULL || file->fs->ops->read == NULL)
+			return -1;
+
+		ret = file->fs->ops->read(file->fs, file->inode, nbyte, file->offset, buf);
+	}
+	file->offset += ret; 
+	return ret;
+
+}
+
+size_t vfs_write(struct file *file, void *buf, size_t nbyte)
+{
+	int ret;
+
+	if((file->type & (FILE_CHAR | FILE_BLOCK)) != 0){
+		ret = device_write(file, buf, file->offset,nbyte);
+	}else{
+		if(file->fs == NULL || file->fs->ops->write == NULL)
+			return -1;
+
+		ret = file->fs->ops->write(file->fs, file->inode, nbyte, file->offset, buf);
+	}
+	file->offset += ret; 
+	return ret;
+
+}
+
+
+//off_t vfs_seek(struct file *
 ssize_t read(int fildes, void *buf, size_t nbyte)
 {
-	UNUSED(fildes);
-	UNUSED(buf);
-	UNUSED(nbyte);
-
-	return -1;
+	struct file *fp = open_files[fildes];
+	if(fp == NULL)
+		return -1;
+	return vfs_read(fp, buf, nbyte);
 }
 ssize_t write(int fildes, void *buf, size_t nbyte)
 {
-	UNUSED(fildes);
-	UNUSED(buf);
-	UNUSED(nbyte);
-
-
-	return -1;
+	struct file *fp = open_files[fildes];
+	if(fp == NULL)
+		return -1;
+	return vfs_write(fp, buf, nbyte);
 }
 /*int creat(const char *path, mode_t mode)*/
 int creat(const char *path, uint32_t mode)
@@ -116,40 +358,9 @@ off_t lseek(int fildes, off_t offset, int whence)
 	UNUSED(whence);
 	return -1;
 }
-#include <stdio.h>
 
-
-int main(int argc, char **argv)
+int sys_chdir(const char *path)
 {
-	UNUSED(argc);
-	UNUSED(argv);
-	if(argc != 3)
-	{
-		printf("invalid number of args!\n");
-		return -1;
-	}
-	if(argv[1][0] != '-')
-	{
-		printf("no flag!\n");
-		return -1;
-	}
-	switch (argv[1][1])
-	{
-		case 'c':
-		case 'l':
-		case 'o':
-		case 'r':
-		case 'w':
-			//printf("not imple\n");
-			ext2(argv[2]);
-			break;
-		default:
-			printf("invalid flag\n");
-		return -1;
-
-
-
-	}
-	return 0;
+	path = path;
+	return -1;
 }
-
--- src/fs/vfs.h
+++ src/fs/vfs.h
@@ -2,26 +2,91 @@
 #define C_OS_VFS_H
 //#include "../include/kernel/types.h"
 /* only define when STDIO is not included */
-#include <stdint.h>
+#include <kernel/types.h>
 #ifndef _STDIO_H
 #define SEEK_SET 0
 #define SEEK_CUR 1
 #define SEEK_END 2
 #endif
-typedef uint8_t * (*vfs_read_inode_t) (void * fs, uint32_t inode, size_t length, size_t offset);
+#define SECTOR_SIZE 512
+
+enum file_type { FILE_CHAR = 1, FILE_BLOCK = 2}; 
+struct file;
+typedef int (*vfs_read_inode_t) (void * fs, uint32_t inode, size_t length, size_t offset, uint8_t *buffer);
 typedef uint32_t (*vfs_write_inode_t) (void *fs, uint32_t inode, size_t length, size_t offset, uint8_t *buffer);
+typedef int (*vfs_read_dir_t) (void *fs, struct file *dir, char *path);
+typedef struct vfs_superblock_ops {
+	vfs_read_inode_t read;
+	vfs_write_inode_t write;
+	vfs_read_dir_t read_dir;
+} vfs_ops_t;
+typedef struct vfs_fs {
+	uint8_t *superblock;
+	uint16_t type;
+	void *aux;
+	vfs_ops_t *ops;
+} vfs_fs_t;
+
+
+
+typedef struct vfs_file {
+	vfs_fs_t *fs;
+	uint32_t inode;//use as cluster number?
+	char *name;//perhaps use name[255/256] instead?NO! what about other fses?
+	
+
+} vfs_file_t;
 
 
 
 
+typedef struct inode_cached {
+	uint32_t group;
+	uint32_t inode;
+} inode_cached_t;
+
+struct file {
+	struct file *parent;
+	//left
+	struct file *dir;
+	//right
+	struct file *file;
+	char name[256];
+	uint8_t type;
+	uint16_t device;
+	uint32_t inode;
+	uint32_t offset;
+	vfs_fs_t *fs;	
+};
+
+extern struct file *root;
+
+
+
+struct file *file_new2(vfs_fs_t *fs, char *name, uint32_t inode);
+struct file *file_new3(char *name, uint8_t type, uint16_t device);
+int insert_file(struct file *dir, struct file *file);
+
+struct file * pathsearch(struct file *dir, char *_path);
+void vfs_init();
 /*int open(const char *path, int oflag, ...);*/
 int open(const char *path, int oflag);
+
 ssize_t read(int fildes, void *buf, size_t nbyte);
 ssize_t write(int filedes, void *buf, size_t nbyte);
 
 /*int creat(const char *path, mode_t mode);*/
-int creat(const char *path, uint32_t mode);
+//int creat(const char *path, uint32_t mode);
 
-off_t lseek(int fildes, off_t offset, int whence);
+//off_t lseek(int fildes, off_t offset, int whence);
+typedef size_t (*device_read_fn)(uint16_t dev, void *buf, off_t offset, size_t count);
+typedef size_t (*device_write_fn)(uint16_t dev, void *buf, off_t offset, size_t count);
 
+
+#define MAJOR(x) ((x & 0xFF00) >> 8)
+#define MINOR(x) (x & 0xFF)
+
+size_t device_read(struct file *file, void *buf, off_t offset, size_t nbyte);
+size_t device_write(struct file *file, void *buf, off_t offset, size_t nbyte);
+void device_file_register(uint16_t dev, device_read_fn read, device_write_fn write);
 #endif
