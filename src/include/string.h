120d0279de29e15e79faa7e0b665f62551d47a80
Bryce Lanham <blanham@gmail.com>
2011-12-06 17:33:15 -0600

    added a bunch of memory/string functions, including strncmp, memset and strtok_r

--- src/include/string.h
+++ src/include/string.h
@@ -4,7 +4,14 @@
 int strlen(char *str);
 
 void *memcpy(void *, const void *, size_t);
+void *memset(void *dest, uint8_t val, size_t count);
 
+char *strcpy(char *dst, const char *src);
 
+char *strcat(char *dst, const char *src);
 int strcmp (const char * str1, const char * str2);
+char * strchr(const char *str, int c);
+int strncmp ( const char * str1, const char * str2, size_t num);
+char * strtok_r (char * str, const char * delimiters, char **save);
+char * strtok (char * str, const char * delimiters);
 #endif
