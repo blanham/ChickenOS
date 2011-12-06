c945610c2c159bad2b5f3860939510301b214e4d
Bryce Lanham <blanham@gmail.com>
2011-12-06 17:33:15 -0600

    added a bunch of memory/string functions, including strncmp, memset and strtok_r

--- src/lib/string.c
+++ src/lib/string.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#define NULL 0
 int strlen(char *str)
 {
 	char *tmp = str;
@@ -16,6 +17,95 @@ int strcmp ( const char * str1, const char * str2 )
 
 	return str1[i] - str2[i]; 
 }
+int strncmp ( const char * str1, const char * str2, size_t num)
+{
+	uint32_t i = 0;	
+
+	for(; str1[i] == str2[i]; i++)
+	{
+		if(str1[i] == '\0')
+			return 0;	
+		if(i == num)
+			return 0;
+	}
+	return str1[i] - str2[i]; 
+}
+
+char * strtok ( char * str, const char * delimiters )
+{
+	str = str;
+	delimiters = delimiters; 
+	
+
+	return NULL;
+}
+char * strchr(const char *str, int c)
+{
+	char *_str = (char *)str;
+
+	while(1)
+		if(*_str == (char)c)
+			return _str;
+		else if(*_str == '\0')
+			return NULL;
+		else
+			_str++;
+
+}
+char * strtok_r ( char * str, const char * delimiters, char **save )
+{
+	char *out;
+
+	if(str == NULL)
+		str = *save;
+	
+	while(strchr(delimiters,*str) != NULL)
+	{
+		if(*str == '\0')
+		{
+			*save = str;
+			return NULL;
+		}	
+		str++;
+	}
+	
+	out = str;
+	
+	while(strchr(delimiters,*str) == NULL)
+		str++;
+	
+	if(*str != '\0')
+	{
+		*str = '\0';
+		*save = str + 1;
+	} else {
+		*save = str;
+	}
+	return out;
+}
+char *strcpy(char *dst, const char *src)
+{
+	char *_src = (char *)src;
+	char *_dst = dst;
+	while(*_src != '\0')
+		*_dst++ = *_src++;
+
+	return dst;
+}
+char * strcat(char *dst, const char * src)
+{
+//	char *d = dst;
+//	char *_src = (char *)src;
+	while(*dst++ != '\0');
+	dst--;
+	strcpy(dst, (char *)src);
+
+
+
+	return dst;
+}
+
+
 void *memcpy(void *dst, const void *src, size_t size)
 {
 	char *_dst = dst;
@@ -27,3 +117,13 @@ void *memcpy(void *dst, const void *src, size_t size)
 
 	return dst;
 }
+void *memset(void *dest, uint8_t val, size_t count)
+{
+	uint8_t *tmp = dest;
+	while(count-- != 0)
+		*tmp++ = val;
+
+	return dest;
+}
+
+
