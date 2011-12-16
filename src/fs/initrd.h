16baaec41cc61fbbeb74abb9da2e0945903bcdd3
Bryce Lanham <blanham@gmail.com>
2011-12-16 15:37:36 -0600

    removed more old files

--- src/fs/initrd.h
+++
@@ -1,5 +0,0 @@
-#include <kernel/types.h>
-void initrd_init(uintptr_t start, uintptr_t end);
-void initrd_tester(char *filename);
-int initrd_read_block(/*initrd_t *disk,*/ uint8_t *dst, uint32_t blocknum);
-
