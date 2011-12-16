523bf5f1087000d90a093b9c0a6c002b3e0305e5
Bryce Lanham <blanham@gmail.com>
2011-12-16 15:32:49 -0600

    minor cleaning of interrupt.c

--- src/include/kernel/interrupt.h
+++ src/include/kernel/interrupt.h
@@ -2,28 +2,36 @@
 #define C_OS_INTERRUPT_H
 
 #include <kernel/types.h>
+
 enum {
 	IRQ0 = 32, IRQ1, IRQ2, IRQ3,
 	IRQ4, IRQ5, IRQ6, IRQ7,
 	IRQ8, IRQ9, IRQ10, IRQ11,
 	IRQ12, IRQ13, IRQ14, IRQ15
 }; 
-struct registers
+
+enum { PIT_I = IRQ0, KBD_I, CASC_I, COM2_I, COM1_I, FD_I};
+enum intr_status {INTR_ENABLED, INTR_DISABLED};
+
+typedef struct registers
 {
   	uint32_t ESP;
 	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax; // Pushed by pusha.
 	uint32_t ds, es, fs, gs;                  // Data segment selectors
 	uint32_t int_no, err_code;    // Interrupt number and error code (if applicable)
 	uint32_t eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
-} __attribute__((packed));
+} __attribute__((packed)) registers_t;
 
-typedef struct registers registers_t;
-
-void dump_regs(struct registers *regs);
-void interrupt_init();
+/*TODO:rename me to registers_dump*/
+void dump_regs(registers_t *regs);
 
 typedef void intr_handler (struct registers *);
+void interrupt_init();
 void interrupt_register(int irq, intr_handler *handler);
 
-enum { PIT_I = IRQ0, KBD_I, CASC_I, COM2_I, COM1_I, FD_I};
+
+enum intr_status interrupt_disable();
+enum intr_status interrupt_enable();
+enum intr_status interrupt_set(enum intr_status);
+enum intr_status interrupt_get();
 #endif
