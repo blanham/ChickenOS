58c7068a49a38e866defc763722617a83a5cee91
Bryce Lanham <blanham@gmail.com>
2011-12-16 15:41:28 -0600

    added liballoc memory allocator

---
+++ src/mm/liballoc_funcs.c
@@ -0,0 +1,23 @@
+#include <kernel/common.h>
+#include <kernel/interrupt.h>
+#include <kernel/types.h>
+#include <kernel/vm.h>
+#include <mm/liballoc.h>
+
+int liballoc_lock() {
+
+        return 0;
+}
+
+int liballoc_unlock() {
+        return 0;
+}
+
+void* liballoc_alloc(size_t pages) {
+        return pallocn(pages);
+}
+
+int liballoc_free(void* ptr, size_t pages) {
+        return pallocn_free(ptr, pages);
+}
+
