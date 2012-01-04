b883cb0dc81634a6684a9efc5083e3c4c786007c
Bryce Lanham <blanham@gmail.com>
2012-01-04 12:47:56 -0600

    commented out lines that caused the cross compiler to complain about an unused variable

--- src/lib/vsprintf.c
+++ src/lib/vsprintf.c
@@ -133,7 +133,7 @@ int linux_vsprintf(char *buf, const char *fmt, va_list args)
 	int field_width;	/* width of output field */
 	int precision;		/* min. # of digits for integers; max
 				   number of chars for from string */
-	int qualifier;		/* 'h', 'l', or 'L' for integer fields */
+	//int qualifier;		/* 'h', 'l', or 'L' for integer fields */
 
 	for (str=buf ; *fmt ; ++fmt) {
 		if (*fmt != '%') {
@@ -181,9 +181,9 @@ int linux_vsprintf(char *buf, const char *fmt, va_list args)
 		}
 
 		/* get the conversion qualifier */
-		qualifier = -1;
+	//	qualifier = -1;
 		if (*fmt == 'h' || *fmt == 'l' || *fmt == 'L') {
-			qualifier = *fmt;
+	//		qualifier = *fmt;
 			++fmt;
 		}
 
