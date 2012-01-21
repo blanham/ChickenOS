dd978b23042399e03bce79e277fc5ba3f5254b2d
Bryce Lanham <blanham@gmail.com>
2012-01-21 17:28:43 -0600

    some work on ata driver, can read mbr off disk

--- src/device/ata.c
+++ src/device/ata.c
@@ -3,6 +3,7 @@
 #include <kernel/console.h>
 #include <kernel/hw.h>
 #include <kernel/serial.h>
+#include <kernel/interrupt.h>
 #include <kernel/vm.h>
 #include <mm/liballoc.h>
 #include <fs/vfs.h>
@@ -18,10 +19,90 @@ enum {ATA_DATA, ATA_FEATURES, ATA_SEC_CNT, ATA_LBALO, ATA_LBAMID, ATA_LBAHI, ATA
 #define DF  0x20
 #define RDY 0x40
 #define BSY 0x80
+struct ata_identify {
+	uint16_t gen_config;
+	uint16_t num_cylinders;
+	uint16_t reserved0;
+	uint16_t num_heads;
+	uint16_t bytes_per_track;
+	uint16_t bytes_per_sector;
+	uint16_t sectors_per_track;
+	uint16_t vendor[3];
+	uint8_t  serial[20];
+	uint16_t buffer_type;
+	uint16_t buffer_size;
+	uint16_t ecc_bytes;
+	uint8_t  firmware_rev[8];
+	uint16_t model_num[40];
+	uint16_t sectors_per_int;
+	uint16_t double_io;
+	uint16_t capabilities;
+	uint16_t reserved1;
+	uint16_t pio_mode;
+	uint16_t dma_mode;
+	uint16_t cyl_lba;
+	uint16_t num_cur_cylinders;
+	uint16_t num_cur_heads;
+	uint16_t num_cur_sectors;
+	uint32_t capacity_in_sectors;
+	uint16_t reserved2;
+	uint32_t user_sectors;
+	uint16_t single_dma;
+	uint16_t multi_dma;
+
+} __attribute__((packed));
+struct partition_entry {
+	uint8_t bootable;
+	uint8_t start_head;
+	unsigned int start_sector :6;
+	unsigned int start_cylinder :10;
+	uint8_t system_id;
+	uint8_t end_head;
+	unsigned int end_sector :6;
+	unsigned int end_cylinder :10;
+	uint32_t rel_sector;
+	uint32_t total_sectors;
+} __attribute__((packed));
+void print_partition_entry(struct partition_entry *entry)
+{
+	printf("bootable %i start head %i start sector %i\n",
+		entry->bootable, entry->start_head, entry->start_sector);
+
+}
+
+struct mbr {
+	uint8_t code[436];
+	uint8_t disk[10];
+//	uint8_t parttable[64];
+	struct partition_entry partitions[4];
+	uint8_t signature[2];
+
+} __attribute__((packed));
+struct ata_drive {
+	uint16_t io_base;
+	struct mbr *mbr;
+};
+struct ata_drive drives[4];
+size_t ata_sector_read(void *buf, unsigned long long lba, uint32_t count UNUSED)
+{
+	uint16_t *data = buf;
+	outb(0x1f6, 0xe0 | ((lba >> 24) & 0xf));
+	outb(0x1f1, 0);
+	outb(0x1f2, 1);
+	outb(0x1f3,lba & 0xff);
+	outb(0x1f4,(lba >> 8) & 0xff);
+	outb(0x1f5,(lba >> 16) & 0xff);
+	outb(0x1f7, 0x20);
+	for(int i = 0; i < 256; i++)
+		data[i] = inw(0x1f0);
+
+	return 0;
+}
 size_t ata_read(uint16_t dev, void *_buf UNUSED, off_t off UNUSED, size_t count UNUSED)
 {
 	uint8_t part = MINOR(dev);
 	printf("trying to read from device %i partiton %i\n",part/64, part);
+	
 	return -1;
 }
 size_t ata_write(uint16_t dev UNUSED, void *_buf UNUSED, off_t off UNUSED, size_t count UNUSED)
@@ -67,23 +148,32 @@ void ata_identify()
 	while((test = inb(0x1f7)) & 0x80);
 	for(int i = 0; i < 256; i++)
 		data[i] = inw(0x1f0);
-	
 	char *p = (void *)&data[27];
-//	uint8_t temp = 0;
 	char *name = kcalloc(41, 1);
-	name[40] = '\0';
 	for(int i = 27; i < 47; i++)
 	{
 		uint16_t temp = data[i];// & 0xff00) >> 8;
 		data[i] = ((temp >> 8) & 0xff) + ((temp << 8) & 0xff00);	
 	}
 	kmemcpy(name, p, 40);
-	name[13] = 0;
-	printf("Detected HD: %s\n",name);	
+//	name[13] = 0;
+	printf("Detected HD: %.14s ",name);
+	struct ata_identify *st = (void *)data;
+	printf("capacity %iMB\n",
+	(st->bytes_per_sector*st->sectors_per_track*st->num_cylinders*st->num_heads)/(1024*1024));
+	uint32_t logical = *(uint32_t *)&data[117];	
+	uint32_t log = *(uint32_t *)&data[60];	
+	printf("size %i\n",logical*log);
 }
 void ata_init()
 {
 	device_register(FILE_BLOCK, 0x300, ata_read, ata_write, ata_ioctl);
+	struct mbr *mbr = kcalloc(sizeof(struct mbr),1);
 	ata_identify();
-	//interrupt_register(0x21, &ata_intr);
+	ata_sector_read(mbr, 0, 1);
+	printf("SIG %X %X\n",mbr->signature[0], mbr->signature[1]);
+	printf("test %i\n",sizeof(struct partition_entry));
+	print_partition_entry(&mbr->partitions[0]);
+
+//interrupt_register(0x21, &ata_intr);
 }
