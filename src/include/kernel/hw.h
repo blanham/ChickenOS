baa21cd3195ffa47a634398cc45a5d6343d0d17f
Bryce Lanham <blanham@gmail.com>
2012-04-28 20:17:50 -0500

    updated port functions in header

--- src/include/kernel/hw.h
+++ src/include/kernel/hw.h
@@ -2,8 +2,11 @@
 #define C_OS_HW_H
 #include <kernel/types.h>
 void outb(uint16_t port, uint8_t value);
+void outw(uint16_t port, uint16_t value);
+void outl(uint16_t port, uint32_t value);
 uint8_t inb(uint16_t port);
 uint16_t inw(uint16_t port);
+uint32_t inl(uint16_t port);
 
 void shutdown();
 #endif
