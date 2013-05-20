a8dd17fbda2925a53dc1d9cb3217866379ee47c6
Bryce Lanham <blanham@gmail.com>
2013-05-19 23:03:45 -0400

    Moved these files, deletes didn't commit last time

--- src/include/kernel/fs/initrd.h
+++
@@ -1,5 +0,0 @@
-#include <kernel/types.h>
-void initrd_init(uintptr_t start, uintptr_t end);
-void initrd_tester(char *filename);
-int initrd_read_block(void *_disk, void *dst, uint32_t blocknum);
-
