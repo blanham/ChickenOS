ab1388cf5cdc41098e042cb050476e3dcdec9149
Bryce Lanham <blanham@gmail.com>
2013-11-07 07:13:56 -0600

    preliminary work on kprintf

---
+++ src/lib/kprintf.c
@@ -0,0 +1,13 @@
+#include <common.h>
+#include <stdarg.h>
+#include <stdio.h>
+//TODO:
+//	always outputs to serial
+//	outputs to console until we hit init
+//	in memory log for dmesg functionality
+void kprintf(char *fmt, ...)
+{
+	(void)fmt;	
+
+
+}
