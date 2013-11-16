a0fa06896672f07f9cb7be193720c63f2168d96d
Bryce Lanham <blanham@gmail.com>
2013-11-16 11:22:12 -0600

    Fixed debug target in Makefile

--- new_update.sh
+++ new_update.sh
@@ -4,9 +4,11 @@ sudo mount -o loop,offset=32256 disk.img ./mnt
 sudo cp src/kernel.bin ./mnt/kernel
 sudo cp grub.cfg ./mnt/boot/grub
 sudo cp init ./mnt/init
+sudo cp dash ./mnt/dash
 sudo cp frotz ./mnt/frotz
 sudo cp zo ./mnt/zo
 sudo cp chicken.bmp ./mnt/chicken.bmp
 sudo cp usrsrc/tests/bin/* ./mnt/tests/
+sudo cp -R disk/* ./mnt/
 sync
 sudo umount ./mnt
