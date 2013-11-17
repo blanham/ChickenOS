54d1086c6e69c98cca57c098b2ad488e81c4dc02
Bryce Lanham <blanham@uchicago.edu>
2013-11-16 23:58:42 -0600

    Added strtol to stdlib

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -3,5 +3,5 @@
 #include <stddef.h>
 void *calloc(size_t num, size_t size);
 void free(void *ptr);
-
+long strtol(const char * restrict str, char ** restrict ptr, int base);
 #endif
--- src/lib/stdlib.c
+++ src/lib/stdlib.c
@@ -9,6 +9,30 @@
 
 void *calloc(size_t num, size_t size)
 {
-
 	return kcalloc(num, size);
 }
+
+void free(void *ptr)
+{
+	kfree(ptr);
+}
+long int strtol(const char* str, char **end, int base)
+{
+	(void)base;
+	long int ret =0;
+	int i, sign;
+	for(i = 0; isspace(str[i]); i++)
+			;
+	sign = (str[i] == '-' ? -1 : 1);
+	if(str[i] == '-' || str[i] == '+')
+		i++;
+	
+	for(ret = 0; isdigit(str[i]); i++)
+	{
+		ret = 10 * ret + (str[i] - '0');
+	}
+	if(end != NULL)
+	 *end = (char *)&str[i];
+	return ret * sign;
+}
+
