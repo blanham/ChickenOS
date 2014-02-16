882cae4f0be3ee0dcdaeaddadea6ff4b497a1299
Bryce Lanham <blanham@gmail.com>
2014-02-16 02:18:23 -0600

    Factored out multiboot stuff, kmain is now clean
    Moved multiboot stuff to a separate file, kmain is now clean of x86
    specific stuff.

--- src/init/kmain.c
+++ src/init/kmain.c
@@ -1,4 +1,6 @@
 #include <common.h>
+#include <stdlib.h>
+#include <chicken/boot.h>
 #include <kernel/hw.h>
 #include <kernel/interrupt.h>
 #include <kernel/thread.h>
@@ -7,7 +9,6 @@
 #include <device/usb.h>
 #include <device/video.h>
 #include <device/audio.h>
-#include <device/tty.h>
 #include <kernel/timer.h>
 #include <mm/vm.h>
 #include <fs/vfs.h>
@@ -17,48 +18,16 @@
 #include <string.h>
 #include <thread/syscall.h>
 
-char *BOOT_MSG = "ChickenOS v0.02 booting\n";
-
-uintptr_t main_loc;
-
+char *BOOT_MSG = "ChickenOS v0.03 booting\n";
 void init(void *aux UNUSED) __attribute__((section(".user"))); 
-extern void modules_init(struct multiboot_info *mb);
-void fork_test();
-extern void multiboot_print(struct multiboot_info *mb);
 
-//TODO: need to change signature since ARM has no multiboot
-//      (unless I write a stub that takes info from u-boot 
-//		and shoves it into a multiboot struct) 
-//mb is already a virtual address
-void kmain(struct multiboot_info* mb, uint32_t magic) 
+void kmain(struct kernel_boot_info *info)
 {
-	main_loc = (uintptr_t)&kmain;
-   	if ( magic != 0x2BADB002 )
-   	{
-	//	console_puts("Bad magic number, halting\r");
-		return;
-   	}
-	
-	#ifdef ARCH_I386
-	/* begin initializations */
-	modules_init(mb);
-/*
-	Instead of the preceding and following functions taking the mb
-	have a function called boot_info_parse that calls accessor functions:
-		vm_init_info
-		modules_init_info
-	tht are boot info agnostic
-	so then you have
-	multiboot_init(mb):
-		struct mem_extents extents
-			^put shit from mb strudt into that
-		vm_init_info(extents)
-	OR
-		just a commandline + parser	
-*/
-	vm_init(mb);
-	
-	tty_init();
+	if(info == NULL)
+		while(1) //Something went wrong, hang
+			;	
+
+	vm_init(info);
 
 	thread_init();
 
@@ -68,18 +37,17 @@ void kmain(struct multiboot_info* mb, uint32_t magic)
 
 	pci_init();
 
-	video_init();
+	video_init(info);
 
 	//we start out with one color scheme
 	//but this will be changed if i ever get a framebuffer
 	//console working
 //	console_set_color(BLUE,WHITE);
 	console_puts(BOOT_MSG);
-//	multiboot_print(mb);
+
 	audio_init();
 	
 	extern void pci_list();
-
 //	pci_list();	
 
 	kbd_init();	
@@ -92,18 +60,16 @@ void kmain(struct multiboot_info* mb, uint32_t magic)
 	
 	usb_init();
 
-
-	//network_init();
+	extern void ata_init();
+	ata_init();	
 
 	vfs_init();
 
+	//network_init();
+	
 	//need to move this back to console.c	
 	console_fs_init();
 
-	extern void ata_init();
-
-	ata_init();	
-	
 	//TODO: move this to a mount_root() function
 	//		can take drive, but should autodetect
 	//		filesytem type from the partition table
@@ -116,32 +82,29 @@ void kmain(struct multiboot_info* mb, uint32_t magic)
 	//so we don't need to switch into user mode	
 	thread_create(NULL,(void *)init, NULL);
 
-	//restarts interrupts, allowing new thread to be
-	//scheduled
-	interrupt_enable();	
-
+	//TODO: We should probably use this thread to schedule
+	//		bottom halves 
+	
 	//should probably be a sleep() or something
 	kernel_halt();
-	
 
 	while(1)
 		;
-	#elif ARCH_ARM
-	(void)mb;
-	while(1)
-		;
-	#endif
 	//should never return, unless things get really fucked
 	PANIC("kmain returned");
 }
-
+//Wut?
+void exit(int code UNUSED)
+{
+	PANIC("exit() called!");
+}
 //FIXME:Here we should do housekeeping, launch any shells or login processes
 //		on the various psuedoterminals, and wait()s on children (which takes
 //		care of zombies processes) 
 void init(void *aux UNUSED) 
 {
-	char *argv[] = {"/busybox","vi",NULL};
-	char *path = "/busybox";
+	char *argv[] = {"/sh", NULL};
+	char *path = "/dash";
 	(void)argv;
 	if(!fork())
 	{
@@ -151,9 +114,9 @@ void init(void *aux UNUSED)
 	//network_setup();
 	//FIXME: probably reuse the above to do a dhcp request
 	//	dummy();
-		SYSCALL_3N(SYS_OPEN, "/dev/tty", 0, NULL);
-		SYSCALL_3N(SYS_OPEN, "/dev/tty", 0, NULL);
-		SYSCALL_3N(SYS_OPEN, "/dev/tty", 0, NULL);
+		SYSCALL_3N(SYS_OPEN, "/dev/tty0", 0, NULL);
+		SYSCALL_3N(SYS_OPEN, "/dev/tty0", 0, NULL);
+		SYSCALL_3N(SYS_OPEN, "/dev/tty0", 0, NULL);
 
 		execv(path, argv);
 		PANIC("execv(init) failed!");
--- src/init/multiboot.c
+++ src/init/multiboot.c
@@ -9,27 +9,127 @@
 #include <device/pci.h>
 #include <device/usb.h>
 #include <mm/liballoc.h>
-/* TODO: Load all data from header into an accesible struct
- *       or supply accessors (e.g. multiboot_get_module() or 
- *		 multiboot_get_cmdline()
- */
+#include <multiboot.h>
+#include <chicken/boot.h>
+#include <chicken/vbe.h>
 
-//FIXME: Move this somewhere else (init/boot.c?)
+struct kernel_boot_info multiboot_storage;
+
+void modules_init(struct multiboot_info *mb);
+
+struct kernel_boot_info *multiboot_parse(struct multiboot_info *mb, uint32_t magic)
+{
+	struct kernel_boot_info *info = &multiboot_storage;
+	struct vbe_mode_info *vbe_info;
+
+	//Do this here for error shit
+	extern void serial_init();	
+	serial_init();
+
+	if(magic != MULTIBOOT_BOOTLOADER_MAGIC)
+		return NULL;
+
+	info->cmdline = (char *)P2V(mb->cmdline);
+	info->aux = mb;
+
+	modules_init(mb);
+
+	if (mb->flags & MULTIBOOT_INFO_MODS)
+	{
+		multiboot_module_t *mod = (multiboot_module_t *) P2V(mb->mods_addr);
+		unsigned i;
+
+		info->modules = (void *)mod;
+		info->modules_count = mb->mods_count;
+
+		for (i = 0; i < mb->mods_count; i++)
+		{
+			 mod->mod_start = P2V(mod->mod_start);
+			 mod->mod_end = P2V(mod->mod_end);
+			 mod->cmdline = P2V(mod->cmdline);
+			 mod++;
+		}
+	}
+
+//	serial_printf("TETST %x\n", P2V(mb->mmap_addr));	
+	vbe_info = (struct vbe_mode_info *)P2V(mb->vbe_mode_info);
+	info->framebuffer = (void *)vbe_info->framebuffer_address; 
+	serial_printf("MMM %x\n", info->framebuffer);
+	info->x_res = vbe_info->x_res;
+	info->y_res = vbe_info->y_res;
+	info->bpp = vbe_info->bpp;
+	info->mode = mb->vbe_mode;
+
+	return info;
+}
+
 void modules_init(struct multiboot_info *mb)
 {
 	extern uint32_t *background_image;
-	
+	//kernel_commandline = (char *)(mb->boot_device);
 	if(mb->mods_count > 0 )
 	{
 		background_image  = (uint32_t *)P2V(*((void**)P2V(mb->mods_addr)));
 	//	uintptr_t end  = (uintptr_t)*((void **)P2V(mb->mods_addr + 4));
+	//	serial_printf("END %X\n", end);
 	//	initrd_init(P2V(start),P2V(end));
 	}
 }
 
-void multiboot_print(struct multiboot_info *mb)
+/*
+void kmain_multiboot(struct multiboot_info * mb UNUSED, uint32_t magic)
 {
-	struct vbe_mode_info {
+	//init polling serial here for printf_serial
+	//
+ 	if ( magic != 0x2BADB002 )
+   	{
+	//	console_puts("Bad magic number, halting\r");
+		return;
+   	}
+	while(1);
+	//have this parse the multiboot header into a COS
+	//specific struct format
+	//struct kernel_info *info = multiboot_parse(mb);
+	//modules_init(mb);
+*/
+/*extern uint32_t *MultiBootHeader;
+
+	printf("MUT %x\n", MultiBootHeader);
+   struct multiboot_header
+     {
+        Must be MULTIBOOT_MAGIC - see above. 
+       multiboot_uint32_t magic;
+     
+       multiboot_uint32_t flags;
+     
+       multiboot_uint32_t checksum;
+     
+       multiboot_uint32_t header_addr;
+       multiboot_uint32_t load_addr;
+       multiboot_uint32_t load_end_addr;
+       multiboot_uint32_t bss_end_addr;
+       multiboot_uint32_t entry_addr;
+
+
+struct multiboot_header *mhdr = (void *)&MultiBootHeader;
+	printf("%x %x %x %x %x\n", mhdr->header_addr, mhdr->load_addr, mhdr->load_end_addr, mhdr->bss_end_addr, mhdr->entry_addr);
+extern void multiboot_print(struct multiboot_info *mb);
+	multiboot_print(mb);
+	while(1);*/
+
+	//kmain(info);
+/*
+
+}
+*/
+
+//FIXME: Move this somewhere else (init/boot.c?)
+void print_mb(unsigned long addr, unsigned long magic);
+
+void print_mb(unsigned long addr, unsigned long magic);
+//void multiboot_print(struct multiboot_info *mb)
+//{
+/*	struct vbe_mode_info {
 		char signature[4];
 		short version;
 		short oemstr[2];
@@ -41,28 +141,65 @@ void multiboot_print(struct multiboot_info *mb)
 		uint32_t prodstr;
 		uint32_t prodrevstr;
 	
-	} *mode_info = kcalloc(sizeof(*mode_info), 1);
-	kmemcpy(mode_info, (void *)P2V(mb->vbe_control_info), sizeof(*mode_info));
-	void *oemstr = (void *)*(uint32_t *)&mode_info->oemstr;
-	printf("Sig %4s\n", mode_info->signature);
- 	printf("Sig %x\n", mode_info->version); 
- 	printf("string %x %x\n", mode_info->oemstr[0],mode_info->oemstr[1]); 
+	} *vbe_info = kcalloc(sizeof(*vbe_info), 1); 
+	struct ModeInfoBlock {
+		uint16_t attributes;
+		uint8_t winA,winB;
+		uint16_t granularity;
+		uint16_t winsize;
+		uint16_t segmentA, segmentB;
+		uintptr_t realFctPtr;
+		uint16_t pitch; // chars per scanline
+			   
+		uint16_t Xres, Yres;
+		uint8_t Wchar, Ychar, planes, bpp, banks;
+		uint8_t memory_model, bank_size, image_pages;
+		uint8_t reserved0;
+		
+		uint8_t red_mask, red_position;
+		uint8_t green_mask, green_position;
+		uint8_t blue_mask, blue_position;
+		uint8_t rsv_mask, rsv_position;
+		uint8_t directcolor_attributes;
+		
+		uint32_t physbase;  // your LFB (Linear Framebuffer) address ;)
+		uint32_t reserved1;
+		short reserved2;
+	} *mode_info;
+	mode_info = (void *)P2V(mb->vbe_mode_info);
+
+	printf("TEST %x\n", mode_info->physbase);
+//	thread_t *cur = thread_current();
+	extern uint32_t * kernel_pd;
+	uintptr_t framebuffer = mode_info->physbase;
+	uint32_t size = mode_info->Xres * mode_info->Yres * mode_info->bpp;
+	printf("X %i Y %i bpp %i SIZE %i\n", mode_info->Xres , mode_info->Yres , mode_info->bpp, size);
+	pagedir_insert_pagen_physical(kernel_pd, (uintptr_t)framebuffer, (uintptr_t)framebuffer, 0x7, size/4096);
+	pagedir_install(kernel_pd);
+	kmemset((void *)framebuffer, 0xAA,size);
+	kmemcpy(vbe_info, (void *)P2V(mb->vbe_control_info), sizeof(*vbe_info));
+	void *oemstr = (void *)*(uint32_t *)&vbe_info->oemstr;
+	printf("Sig %4s\n", vbe_info->signature);
+ 	printf("Sig %x\n", vbe_info->version); 
+ 	printf("string %x %x\n", vbe_info->oemstr[0],vbe_info->oemstr[1]); 
 	printf("Strinf %x\n", (uint32_t *)oemstr);
- 	printf("string %x %x\n", mode_info->vendstr[0],mode_info->vendstr[1]); 
-printf("%x %x %x %x %x %x\n", mb->vbe_control_info,
+ 	printf("string %x %x\n", vbe_info->vendstr[0],vbe_info->vendstr[1]); 
+ 	printf("Pointer? %.4x%.4x\n", vbe_info->videomodes[0] & 0xFFFF,vbe_info->videomodes[1] & 0xFFFF);*/ 
+/*printf("Control info %x  mode info %x mode %x seg %x off %x len %x\n", mb->vbe_control_info,
         mb->vbe_mode_info,
         mb->vbe_mode,
         mb->vbe_interface_seg,
         mb->vbe_interface_off,
-        mb->vbe_interface_len);
+        mb->vbe_interface_len);*/
 
+//	print_mb((unsigned long)mb, 0x2BADB002);
 
-}
+//}
 //this needs to go somewhere else
 void print_mb(unsigned long addr, unsigned long magic)
 {
      #define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
-	addr = P2V(addr);
+	//addr = P2V(addr);
  /* Am I booted by a Multiboot-compliant boot loader? */
        if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
          {
@@ -101,7 +238,7 @@ void print_mb(unsigned long addr, unsigned long magic)
            for (i = 0, mod = (multiboot_module_t *) P2V(mbi->mods_addr);
                 i < mbi->mods_count;
                 i++, mod++)
-             printf (" mod_start = 0x%x, mod_end = 0x%x, cmdline = %s\n",
+             printf (" mod_start = 0x%x, mod_end = 0x%x, cmdline = %p\n",
                      (unsigned) P2V(mod->mod_start),
                      (unsigned) P2V(mod->mod_end),
                      (char *) P2V(mod->cmdline));
@@ -136,27 +273,25 @@ void print_mb(unsigned long addr, unsigned long magic)
                    (unsigned) multiboot_elf_sec->num, (unsigned) multiboot_elf_sec->size,
                    (unsigned) P2V(multiboot_elf_sec->addr), (unsigned) multiboot_elf_sec->shndx);
          }
-return;
+//return;
       /* Are mmap_* valid? */
        if (CHECK_FLAG (mbi->flags, 6))
          {
-          // multiboot_memory_map_t *mmap;
+           multiboot_memory_map_t *mmap;
      
            printf ("mmap_addr = 0x%x, mmap_length = 0x%x\n",
                    (unsigned) mbi->mmap_addr, (unsigned) mbi->mmap_length);
-       /*    for (mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
-                (unsigned long) mmap < mbi->mmap_addr + mbi->mmap_length;
+           for (mmap = (multiboot_memory_map_t *) P2V(mbi->mmap_addr);
+                (unsigned long) mmap < P2V(mbi->mmap_addr + mbi->mmap_length);
                 mmap = (multiboot_memory_map_t *) ((unsigned long) mmap
                                          + mmap->size + sizeof (mmap->size)))
-             printf (" size = 0x%x, base_addr = 0x%x%x,"
-                     " length = 0x%x%x, type = 0x%x\n",
+             printf (" size = 0x%x, base_addr = 0x%x,"
+                     " length = 0x%x, type = 0x%x\n",
                      (unsigned) mmap->size,
-                     (unsigned) mmap->base_addr_high,
-                     (unsigned) mmap->base_addr_low,
-                     (unsigned) mmap->length_high,
-                     (unsigned) mmap->length_low,
+                     (unsigned) mmap->addr,
+                     (unsigned) mmap->len,
                      (unsigned) mmap->type);
-        */ 
+         
 		} 
 
 }
