e7a49baaad5bf5f5ef93006cd3f39dcf07ee0625
Bryce Lanham <blanham@gmail.com>
2015-12-26 01:13:30 -0600

    RTC code is now easier to read

--- src/device/timer.c
+++ src/device/timer.c
@@ -1,5 +1,8 @@
-/* PIT and RTC routines */
-//TODO: Move much of this to arch/i386/pit.c and device/time.c
+/* ChickenOS PIT and RTC routines
+ *
+ * TODO: Move much of this to arch/i386/pit.c and device/time.c
+ *
+ */
 #include <kernel/timer.h>
 #include <kernel/interrupt.h>
 #include <kernel/common.h>
@@ -16,103 +19,89 @@
 #define RTC_REG    0x70
 #define RTC_DATA   0x71
 #define RTC_UPDATE 0x80
-uint8_t rtc_data[8];
+
+#define BCD_TO_DEC(x) (((x / 16) * 10) + (x & 0xf))
+
+struct c_os_time system_datetime;
+int rtc_format = 0;
 uint32_t ticks = 0;
 uint32_t unix_time;
 
-struct c_os_time {
-	uint8_t hour;
-	uint8_t minute;
-	uint8_t sec;
-	uint8_t month;
-	uint8_t day;
-	uint8_t year;
-	uint8_t weekday;
-	uint8_t century;
-
-};
-#define BCD_TO(x) (((x / 16) * 10) + (x & 0xf))
 char * days[7] = {
-	"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
+	"Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
 };
+
 uint8_t read_rtc_reg(uint8_t reg)
 {
-/*	int chk = 0;
+/*
+	int chk = 0;
 start:
 	outb(RTC_REG, 0xA);
 	chk = inb(RTC_REG);
 	if(!(chk&RTC_UPDATE))
 		goto start;
-*/	outb(RTC_REG, reg);
+*/
+	outb(RTC_REG, reg);
 
-	return inb(RTC_DATA);
-
+	uint8_t ret = inb(RTC_DATA);
+	if (rtc_format == 2)
+		return BCD_TO_DEC(ret);
+	return ret;
 }
-/* gregorian date to julian day function
- * taken from:http://www.hermetic.ch/cal_stud/jdn.htm#comp
- */
+
+// Gregorian date to Julian day function
+// taken from:http://www.hermetic.ch/cal_stud/jdn.htm#comp
 int32_t jdt(int8_t m, int8_t d, int16_t y)
 {
-int32_t jd = ( 1461 * ( y + 4800 + ( m - 14 ) / 12 ) ) / 4 +
-          ( 367 * ( m - 2 - 12 * ( ( m - 14 ) / 12 ) ) ) / 12 -
-          ( 3 * ( ( y + 4900 + ( m - 14 ) / 12 ) / 100 ) ) / 4 +
-          d - 32075;
+	int32_t jd = ( 1461 * ( y + 4800 + ( m - 14 ) / 12 ) ) / 4 +
+		( 367 * ( m - 2 - 12 * ( ( m - 14 ) / 12 ) ) ) / 12 -
+		( 3 * ( ( y + 4900 + ( m - 14 ) / 12 ) / 100 ) ) / 4 +
+		d - 32075;
 
-return jd;
-
-
+	return jd;
 }
 
-void rtc_data_print(uint8_t *data)
+void time_print(struct c_os_time *time)
 {
-	printf("%s ",days[data[3]]);
-	printf("%i/%i/%i  ", data[5], data[4], data[6]);
-	printf("%i:%i:%i ", data[2], data[1], data[0]);
-//	uint32_t timestamp = (data[7]+30)*31556926;
-//	timestamp +=
- 
-	uint32_t m,d,y;
-	m = data[5];
-	d = data[4];
-	y = data[6] + 2000;
-	//uint32_t test = jdt(11,22,2011);
-	uint32_t test2 = (jdt(m,d,y) - jdt(1,1,1970));
-	uint32_t test3 = data[2]*60*60 + data[1]*60 + data[0]; 
-	printf("timestamp: %i\n",(test2)* 24 * 60 * 60 + test3);
-	unix_time = (test2* 24 * 60 * 60) + test3;
+	printf("%s ",days[time->weekday]);
+	printf("%i/%i/%i ", time->month, time->day, time->year+2000);
+	printf("%i:%i:%i\n", time->hour, time->minute, time->second);
 }
+
+void time_set_from_rtc(struct c_os_time *time)
+{
+	uint32_t julian_day = jdt(time->month, time->day,time->year+2000);
+	uint32_t days_since_epoch = (julian_day - jdt(1,1,1970));
+	uint32_t seconds_since_midnight = time->hour*60*60 + time->minute*60 + time->second;
+	unix_time = (days_since_epoch * 24 * 60 * 60) + seconds_since_midnight;
+	time->weekday = (julian_day + 1) % 7;
+	time_print(time);
+}
+
 void rtc_init()
 {
+	rtc_format = read_rtc_reg(0xB);
 
-	int format = read_rtc_reg(0xB);
-	
-	rtc_data[0] = read_rtc_reg(0);
-	rtc_data[1] = read_rtc_reg(2);
-	rtc_data[2] = read_rtc_reg(4);
+	system_datetime.second = read_rtc_reg(0);
+	system_datetime.minute = read_rtc_reg(2);
+	system_datetime.hour = read_rtc_reg(4);
 	//Should be Weekday, doesn't work correctly
-	//rtc_data[3] = read_rtc_reg(6);
-		
-	rtc_data[4] = read_rtc_reg(7);
-	rtc_data[5] = read_rtc_reg(8);
-	rtc_data[6] = read_rtc_reg(9);
-	rtc_data[7] = read_rtc_reg(0x32);
-	if(format == 2)
-		for(int i = 0; i < 7; i++)
-			rtc_data[i] = BCD_TO(rtc_data[i]);
-	if(rtc_data[2] == 0) rtc_data[2] = 12;
-	if(rtc_data[2] > 12) rtc_data[2] -= 12;
+	system_datetime.weekday = read_rtc_reg(6);
 
-	rtc_data_print(rtc_data);
+	system_datetime.day = read_rtc_reg(7);
+	system_datetime.month = read_rtc_reg(8);
+	system_datetime.year = read_rtc_reg(9);
+	system_datetime.century = read_rtc_reg(0x32);
+	//XXX: Is this right?
+	if(system_datetime.hour == 0) system_datetime.hour = 12;
+	if(system_datetime.hour > 12) system_datetime.hour -= 12;
 
-
+	time_set_from_rtc(&system_datetime);
 }
 
 void timer_intr(struct registers * regs)
 {
 	(void)regs;
-//	dump_regs(regs);
-//	dump_regs(regs);
-//	printf("fuck\n");
 	ticks++;
 	if(ticks % 100 == 0)
 	{
@@ -124,11 +113,11 @@ void timer_intr(struct registers * regs)
 void timer_init(uint32_t frequency)
 {
 	int div = 1193180 / frequency;
-	
+
 	outb(PIT_CMD, 0x36);
 	outb(PIT0_DATA, div & 0xFF);
 	outb(PIT0_DATA, div >> 8);
-	
+
 	interrupt_register(IRQ0, &timer_intr);
 }
 
@@ -165,15 +154,15 @@ int sys_gettimeofday(struct timeval *tp, void *tzp UNUSED)
 	{
 		tp->tv_sec = unix_time;
 		tp->tv_usec = (ticks % 100) * 100;
-		
-	}	
+	}
 
 	return 0;
 }
+
 int sys_clock_gettime(int type, struct timespec *tp)
 {
 	(void)type;
-	static int s = 0; 
+	static int s = 0;
 	tp->tv_sec = unix_time;
 	tp->tv_nsec = s;
 	s += 100;
--- src/include/kernel/timer.h
+++ src/include/kernel/timer.h
@@ -1,6 +1,19 @@
 #ifndef C_OS_TIMER_H
 #define C_OS_TIMER_H
+#include <stdint.h>
 #include <sys/time.h>
+
+struct c_os_time {
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+	uint8_t month;
+	uint8_t day;
+	uint8_t year;
+	uint8_t weekday;
+	uint8_t century;
+};
+
 void time_init();
 int sys_gettimeofday(struct timeval *tp, void *tzp);
 void time_sleep(int seconds);
